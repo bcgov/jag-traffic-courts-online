250cbd7d3d2708d9a32340e667e2fe642dce79bd
Ranti Familusi <ranti.familusi@nttdata.com>
2021-01-29 13:27:03 -0700

    add comment to configuration extension

--- src/backend/TrafficCourtsApi/DisputeApi.Web/Features/Tickets/Configuration/TicketServiceConfigurationExtension.cs
+++ src/backend/TrafficCourtsApi/DisputeApi.Web/Features/Tickets/Configuration/TicketServiceConfigurationExtension.cs
@@ -3,6 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace DisputeApi.Web.Features.Tickets.Configuration
 {
+    /// <summary>
+    /// Extension to inject ticket configuration service in service collection
+    /// </summary>
     public static class TicketServiceConfigurationExtension
     {
         public static void AddTicketService(this IServiceCollection collection)
