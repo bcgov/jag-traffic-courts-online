621a4ce99b8ed2cfafafedf73c0542897aae7e88
PeggyZhangNtt <190728@nttdata.com>
2021-02-09 10:43:26 -0800

    clean up

--- src/frontend/citizen-portal/src/app/modules/dispute/components/start/start.component.ts
+++ src/frontend/citizen-portal/src/app/modules/dispute/components/start/start.component.ts
@@ -2,15 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder } from '@angular/forms';
 import { Subscription, timer } from 'rxjs';
-import moment from 'moment';
 
 import { BaseDisputeFormPage } from '@dispute/classes/BaseDisputeFormPage';
 import { DisputeResourceService } from '@dispute/services/dispute-resource.service';
 import { DisputeService } from '@dispute/services/dispute.service';
-import { LoggerService } from '@core/services/logger.service';
-import { ToastService } from '@core/services/toast.service';
-import { FormUtilsService } from '@core/services/form-utils.service';
-import { UtilsService } from '@core/services/utils.service';
+
 
 @Component({
   selector: 'app-start',
@@ -19,7 +15,6 @@ import { UtilsService } from '@core/services/utils.service';
 })
 export class StartComponent extends BaseDisputeFormPage implements OnInit {
   public busy: Subscription;
-  public maxViolationDate: moment.Moment;
   public pageMode: string;
 
   constructor(
@@ -28,10 +23,6 @@ export class StartComponent extends BaseDisputeFormPage implements OnInit {
     protected formBuilder: FormBuilder,
     protected disputeService: DisputeService,
     protected disputeResource: DisputeResourceService,
-    private toastService: ToastService,
-    private formUtilsService: FormUtilsService,
-    private utilsService: UtilsService,
-    private logger: LoggerService
   ) {
     super(route, router, formBuilder, disputeService, disputeResource);
     this.pageMode = 'full';
