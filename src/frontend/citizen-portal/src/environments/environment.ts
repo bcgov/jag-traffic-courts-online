fcca10c6c084c0a66c89d63af886421719a23ab5
Ranti Familusi <ranti.familusi@nttdata.com>
2021-02-10 15:00:20 -0700

    docker compose update

--- src/frontend/citizen-portal/src/environments/environment.ts
+++ src/frontend/citizen-portal/src/environments/environment.ts
@@ -1,22 +1,19 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-console.log(window["env"]["useKeycloak"]);
+
 export const environment = {
-  production: false,
-  environmentName: "local-dev",
-  version: '1.0.0',
-  useKeycloak: false,
-  useMockServices:  window["env"]["useKeycloak"],
-  apiUrl: 'http://localhost:4300/api',
-  keycloakConfig: {
-    config: {
-      url: 'http://localhost:8081/auth',
-      realm: 'tco',
-      clientId: 'tco-client',
-    },
-    initOptions: {
-      onLoad: 'check-sso',
-    },
-  }
-};
+    production: false,
+    environmentName: "demo",
+    version: "1.0.0",
+    useKeycloak: "false",
+    useMockServices: "false",
+    apiUrl: "http://localhost:4300/api",
+    keycloakConfig: {
+        config: {
+          url: "http://localhost:8081/auth",
+          realm: "traffic-court",
+          clientId: "test",
+        },
+        initOptions: {
+          onLoad: "check-sso",
+        },
+      }
+}
