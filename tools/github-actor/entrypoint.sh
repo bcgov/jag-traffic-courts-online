9824c3e4496a09eb49d0d55678c107f5739e207e
Paul Martin <paul12.martin@nttdata.com>
2021-03-09 16:43:55 -0700

    switching to RHEL image

--- tools/github-actor/entrypoint.sh
+++ tools/github-actor/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
-echo "Requesting registration URL at '${registration_url}'"
+echo "Requesting registration URL at ${registration_url}"
 
 payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
