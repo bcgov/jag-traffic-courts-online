8127a2a53c00484d7cb21fe005d45d2919290c5d
PeggyZhangNtt <190728@nttdata.com>
2021-04-08 14:45:45 -0700

    respond to review

--- src/backend/TrafficCourtsApi/DisputeApi.Web/Auth/OAuthClient.cs
+++ src/backend/TrafficCourtsApi/DisputeApi.Web/Auth/OAuthClient.cs
@@ -27,9 +27,6 @@ namespace DisputeApi.Web.Auth
 
         public async Task<Token> GetRefreshToken(CancellationToken cancellationToken)
         {
-            _httpClient.DefaultRequestHeaders.Add("return-client-request-id", "true");
-            _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
-
             var data = new Dictionary<string, string>
             {
                 {"resource", _oAuthOptions.ResourceUrl},
@@ -41,28 +38,26 @@ namespace DisputeApi.Web.Auth
             };
 
             var content = new FormUrlEncodedContent(data);
-            using (var request = new HttpRequestMessage(HttpMethod.Post, _oAuthOptions.OAuthUrl) { Content = content })
+            using var request = new HttpRequestMessage(HttpMethod.Post, _oAuthOptions.OAuthUrl) { Content = content };
+            request.Headers.Add("return-client-request-id", "true");
+            request.Headers.Add("Accept", "application/json");
+            var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead,
+                cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
             {
-                var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead,
-                    cancellationToken);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    var responseData = response.Content == null
-                        ? null
-                        : await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                    _logger.LogError($"get oauth token failed. {responseData}"); 
-                    throw new OAuthException(
-                        "The HTTP status code of the response was not expected (" + (int)response.StatusCode + ").",
-                        (int)response.StatusCode, responseData,
-                        response.Headers.ToDictionary(x => x.Key, x => x.Value), null);
-                }
-
-                _logger.LogInformation("get oauth token successfully");
-                var stream = await response.Content.ReadAsStreamAsync();
-
-                return await JsonSerializer.DeserializeAsync<Token>(stream);
+                var responseData = await response.Content.ReadAsStringAsync(cancellationToken).ConfigureAwait(false);
+                _logger.LogError("get oauth token failed. {responseData}", responseData); 
+                throw new OAuthException(
+                    "The HTTP status code of the response was not expected (" + (int)response.StatusCode + ").",
+                    (int)response.StatusCode, responseData,
+                    response.Headers.ToDictionary(x => x.Key, x => x.Value), null);
             }
+
+            _logger.LogInformation("get oauth token successfully");
+            var stream = await response.Content.ReadAsStreamAsync(cancellationToken);
+
+            return await JsonSerializer.DeserializeAsync<Token>(stream);
         }
     }
 }
