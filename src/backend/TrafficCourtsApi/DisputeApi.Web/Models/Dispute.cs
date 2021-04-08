8366af04a7afeae8ac4d9cbde216a8392b9b5598
PeggyZhangNtt <190728@nttdata.com>
2021-04-08 15:31:08 -0700

    fix

--- src/backend/TrafficCourtsApi/DisputeApi.Web/Models/Dispute.cs
+++ src/backend/TrafficCourtsApi/DisputeApi.Web/Models/Dispute.cs
@@ -11,15 +11,15 @@ namespace DisputeApi.Web.Models
         public int OffenceNumber { get; set; }
         public string EmailAddress { get; set; }
         public OffenceAgreementStatus OffenceAgreementStatus { get; set; }
-        public bool? RequestReduction { get; set; }
-        public bool? RequestMoreTime { get; set; }
+        public bool RequestReduction { get; set; }
+        public bool RequestMoreTime { get; set; }
         public string ReductionReason { get; set; }
         public string MoreTimeReason { get; set; }
-        public bool? LawyerPresent { get; set; }
-        public bool? InterpreterRequired { get; set; }
-        public bool? WitnessPresent { get; set; }
+        public bool LawyerPresent { get; set; }
+        public bool InterpreterRequired { get; set; }
+        public bool WitnessPresent { get; set; }
         public string InterpreterLanguage { get; set; }
-        public bool? InformationCertified { get; set; }
+        public bool InformationCertified { get; set; }
         public DisputeStatus Status { get; set; }
     }
 
