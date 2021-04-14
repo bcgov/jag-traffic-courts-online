e178cec55ddb14b95071c77d3deecbd481e74ad8
PeggyZhangNtt <190728@nttdata.com>
2021-04-13 17:01:07 -0700

    update rsi info

--- src/backend/TrafficCourtsApi/DisputeApi.Web/Keys.cs
+++ src/backend/TrafficCourtsApi/DisputeApi.Web/Keys.cs
@@ -7,8 +7,13 @@ namespace DisputeApi.Web
 {
     public static class Keys
     {
-        public static readonly string OAUTH_TOKEN_KEY = "oauth-token";
-        public static readonly int OAUTH_TOKEN_EXPIRE_BUFFER = 60;
-        public static string RSI_OPERATION_MODE_FAKE = "FAKE";
+        public static readonly string OauthTokenKey = "oauth-token";
+        public static readonly int OauthTokenExpireBuffer = 60;
+        public static string RsiOperationModeFake = "FAKE";
+        public static string Nothing = "n/a";
+        public static int TicketDiscountValidDays =
+            Environment.GetEnvironmentVariable("TICKET_DISCOUNT_VALID_DAYS") == null
+                ? 30
+                : Convert.ToInt32(Environment.GetEnvironmentVariable("TICKET_DISCOUNT_VALID_DAYS"));
     }
 }
