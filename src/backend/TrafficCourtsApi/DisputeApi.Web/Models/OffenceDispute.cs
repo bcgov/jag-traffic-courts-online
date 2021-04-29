d4f0f748e0ee32de2a77eae6f54c7efc33ad4426
PeggyZhangNtt <190728@nttdata.com>
2021-04-29 15:12:11 -0700

    Auto stash before checking out "origin/main"

---
+++ src/backend/TrafficCourtsApi/DisputeApi.Web/Models/OffenceDispute.cs
@@ -0,0 +1,18 @@
+﻿namespace DisputeApi.Web.Models
+{
+    public class OffenceDispute
+    {
+        public string ViolationTicketNumber { get; set; }
+        public string ViolationTime { get; set; }
+        public string ViolationDate { get; set; }
+        public bool InformationCertified { get; set; }
+        public Additional Additional { get; set; }
+        public OffenceDisputeDetail OffenceDisputeDetail { get; set; }
+
+    }
+
+
+ 
+
+ 
+}
---
+++ src/backend/TrafficCourtsApi/DisputeApi.Web/Models/OffenceDisputeDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DisputeApi.Web.Models
+{
+    public class OffenceDisputeDetail
+    {
+        public TrafficCourts.Common.Contract.OffenceAgreementStatus OffenceAgreementStatus { get; set; }
+        public bool RequestReduction { get; set; }
+        public bool RequestMoreTime { get; set; }
+        public string ReductionReason { get; set; }
+        public string MoreTimeReason { get; set; }
+        public TrafficCourts.Common.Contract.DisputeStatus Status { get; set; }
+    }
+}
