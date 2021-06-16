5b16f6a1d8242a9cf1ee23f15e1d4a619efe1997
Carol Carpenter <carol.carpenter@gov.bc.ca>
2021-06-16 16:38:20 -0700

    Reverse landing page image,  create default link values for 'Visit site' links

--- src/frontend/citizen-portal/src/app/services/app-config.service.ts
+++ src/frontend/citizen-portal/src/app/services/app-config.service.ts
@@ -10,6 +10,10 @@ export class AppConfig {
   understandYourTicketLink: string;
   paymentOptionsLink: string;
   resolutionOptionsLink: string;
+  roadSafetyBCVisitUsLink: string;
+  icbcVisitUsLink: string;
+  provincialCourtOfBCVisitUsLink: string;
+  courthouseServicesOfBCVisitUsLink: string;
 }
 
 @Injectable({
@@ -17,6 +21,15 @@ export class AppConfig {
 })
 export class AppConfigService {
   private appConfig: AppConfig;
+
+  private RSBC_VISIT_US_DEFAULT =
+    'https://www2.gov.bc.ca/gov/content/transportation/driving-and-cycling/roadsafetybc/intersection-safety-cameras' as const;
+  private ICBC_VISIT_US_DEFAULT =
+    'https://www.icbc.com/driver-licensing/tickets/Pages/default.aspx' as const;
+  private PROV_CRT_VISIT_US_DEFAULT =
+    'https://www.provincialcourt.bc.ca/types-of-cases/traffic-and-bylaw-matters' as const;
+  private CTH_SERV_VISIT_US_DEFAULT =
+    'https://www2.gov.bc.ca/gov/content/justice/courthouse-services/fines-payments' as const;
 
   constructor(private http: HttpClient) {}
 
@@ -60,5 +73,37 @@ export class AppConfigService {
 
   get resolutionOptionsLink(): string {
     return this.appConfig?.resolutionOptionsLink;
+  }
+
+  get roadSafetyBCVisitUsLink(): string {
+    const link = this.appConfig?.roadSafetyBCVisitUsLink;
+    if (link) {
+      return link;
+    }
+    return this.RSBC_VISIT_US_DEFAULT;
+  }
+
+  get icbcVisitUsLink(): string {
+    const link = this.appConfig?.icbcVisitUsLink;
+    if (link) {
+      return link;
+    }
+    return this.ICBC_VISIT_US_DEFAULT;
+  }
+
+  get provincialCourtOfBCVisitUsLink(): string {
+    const link = this.appConfig?.provincialCourtOfBCVisitUsLink;
+    if (link) {
+      return link;
+    }
+    return this.PROV_CRT_VISIT_US_DEFAULT;
+  }
+
+  get courthouseServicesOfBCVisitUsLink(): string {
+    const link = this.appConfig?.courthouseServicesOfBCVisitUsLink;
+    if (link) {
+      return link;
+    }
+    return this.CTH_SERV_VISIT_US_DEFAULT;
   }
 }
