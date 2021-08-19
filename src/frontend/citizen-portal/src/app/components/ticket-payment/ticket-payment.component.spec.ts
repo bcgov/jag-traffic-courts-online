664890ae3aa5bae916a7a96699fb3a0920ec0f8b
Carol Carpenter <carol.carpenter@gov.bc.ca>
2021-08-19 14:49:45 -0700

    Fix unit test

--- src/frontend/citizen-portal/src/app/components/ticket-payment/ticket-payment.component.spec.ts
+++ src/frontend/citizen-portal/src/app/components/ticket-payment/ticket-payment.component.spec.ts
@@ -1,8 +1,10 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.module';
 
 import { TicketPaymentComponent } from './ticket-payment.component';
@@ -15,10 +17,12 @@ describe('TicketPaymentComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule,
+        ReactiveFormsModule,
         RouterModule.forRoot([]),
         RouterTestingModule,
         BrowserAnimationsModule,
         NgxMaterialModule,
+        TranslateModule.forRoot(),
       ],
       declarations: [TicketPaymentComponent],
     }).compileComponents();
