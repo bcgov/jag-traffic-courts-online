b724fae3c84c74f552e6218c145c86ae153f1a09
Carol Carpenter <carol.carpenter@gov.bc.ca>
2021-08-24 08:03:13 -0700

    Change title and update error handing

--- src/frontend/citizen-portal/src/app/core/services/error-handler.service.ts
+++ src/frontend/citizen-portal/src/app/core/services/error-handler.service.ts
@@ -5,15 +5,14 @@ import { Router } from '@angular/router';
 import { LoggerService } from '@core/services/logger.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ErrorHandlerService implements ErrorHandler {
-
   constructor(
     // Can't use DI in constructor of error handler since it's
     // loaded first therefore have to use the injector
     private injector: Injector
-  ) { }
+  ) {}
 
   public handleError(error: Error | HttpErrorResponse) {
     const logger = this.injector.get(LoggerService);
@@ -26,18 +25,18 @@ export class ErrorHandlerService implements ErrorHandler {
         return logger.error('No Internet Connection');
       } else {
         // HTTP error has occurred (error.status = 403, 404, 500...)
-        return logger.error(`${error.status} - ${error.message}`);
+        return logger.error(
+          `HTTP error has occurred ${error.status} - ${error.message}`
+        );
       }
     } else {
       // Client error has occurred (Angular Error, ReferenceError...)
     }
 
-    const message = (error.message)
-      ? error.message
-      : error.toString();
+    const message = error.message ? error.message : error.toString();
     const url = router.url;
 
-    logger.error(message, { url });
+    logger.error('ErrorHandlerService::handleError', message, { url });
 
     // IMPORTANT: Rethrow the error, otherwise it gets swallowed
     throw error;
