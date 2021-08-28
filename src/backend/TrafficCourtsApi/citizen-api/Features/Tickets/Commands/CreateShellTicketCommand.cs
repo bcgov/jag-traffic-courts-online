76136710eee054a9ca48915186a2a76fcb1067b0
peggy-gov-bc-ca <190728@nttdata.com>
2021-08-27 17:11:45 -0700

    image upload
    test in openshift
    
    
    test gitaction
    
    
    fix logging config
    
    
    save image to file system
    
    
    t

--- src/backend/TrafficCourtsApi/citizen-api/Features/Tickets/Commands/CreateShellTicketCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using Gov.CitizenApi.Models;
-using MediatR;
-
-namespace Gov.CitizenApi.Features.Tickets.Commands
-{
-    public class CreateShellTicketCommand : ShellTicket, IRequest<CreateShellTicketResponse>
-    {
-    }
-}
--- src/backend/TrafficCourtsApi/citizen-api/Features/Tickets/Commands/CreateShellTicketResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Gov.CitizenApi.Features.Tickets.Commands
-{
-    public class CreateShellTicketResponse
-    {
-        public int Id { get; set; }
-    }
-}
