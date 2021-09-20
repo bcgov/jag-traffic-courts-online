54dc7384bcd962edd8b84e10e14e25cd1e0b0919
peggy-gov-bc-ca <190728@nttdata.com>
2021-09-20 13:08:07 -0700

    splunk log and first dashboard
    test
    
    
    setting
    
    
    update source type
    
    
    add splunk-config secrets
    
    
    add ticket-number to log context
    
    
    temp
    
    
    initial setup for splunk

--- src/backend/TrafficCourtsApi/citizen-api/Features/Tickets/TicketsController.cs
+++ src/backend/TrafficCourtsApi/citizen-api/Features/Tickets/TicketsController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NSwag.Annotations;
+using Serilog.Context;
 
 namespace Gov.CitizenApi.Features.Tickets
 {
@@ -36,20 +37,23 @@ namespace Gov.CitizenApi.Features.Tickets
         [ProducesResponseType(typeof(ApiMessageResponse), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Ticket([FromQuery]TicketSearchQuery query)
         {
-            try
+            using (LogContext.PushProperty("TicketNumber", query.TicketNumber))
             {
-                _logger.LogInformation("get ticket search query.");
-                var response = await _mediator.Send(query);
-                return response == null ? NoContent() : Ok(ApiResponse.Result(response));
-            }
-            catch (TicketSearchException exception) when (exception.StatusCode == 204)
-            {
-                return NoContent();
-            }
-            catch(Exception e)
-            {
-                _logger.LogError(e, "GetTicket failed");
-                return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Message(e.Message));
+                try
+                {
+                    _logger.LogInformation("get ticket search query.");
+                    var response = await _mediator.Send(query);
+                    return response == null ? NoContent() : Ok(ApiResponse.Result(response));
+                }
+                catch (TicketSearchException exception) when (exception.StatusCode == 204)
+                {
+                    return NoContent();
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "GetTicket failed");
+                    return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Message(e.Message));
+                }
             }
         }
 
@@ -59,22 +63,25 @@ namespace Gov.CitizenApi.Features.Tickets
         [ProducesResponseType(typeof(ApiMessageResponse), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> ShellTicket([FromBody]CreateShellTicketCommand createShellTicket)
         {
-            try
+            using (LogContext.PushProperty("TicketNumber", createShellTicket.ViolationTicketNumber))
             {
-                _logger.LogInformation("get create shell ticket request.");
-                var response = await _mediator.Send(createShellTicket);
-                if(response.Id == 0)
+                try
                 {
-                    ModelState.AddModelError("TicketNumber", "This ticket already exists.");
-                    return BadRequest(ApiResponse.BadRequest(ModelState));
+                    _logger.LogInformation("get create shell ticket request.");
+                    var response = await _mediator.Send(createShellTicket);
+                    if (response.Id == 0)
+                    {
+                        ModelState.AddModelError("TicketNumber", "This ticket already exists.");
+                        return BadRequest(ApiResponse.BadRequest(ModelState));
+                    }
+                    return RedirectToAction("Ticket", new { ticketNumber = createShellTicket.ViolationTicketNumber, time = createShellTicket.ViolationTime });
+                    //return Ok();
                 }
-                return RedirectToAction("Ticket", new { ticketNumber = createShellTicket.ViolationTicketNumber, time = createShellTicket.ViolationTime });
-                //return Ok();
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "create shell ticket failed");
-                return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Message(e.Message));
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "create shell ticket failed");
+                    return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Message(e.Message));
+                }
             }
         }
 
@@ -85,16 +92,19 @@ namespace Gov.CitizenApi.Features.Tickets
         [ProducesResponseType(typeof(ApiMessageResponse), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Pay([FromQuery] TicketPaymentCommand ticketPayment)
         {
-            try
+            using (LogContext.PushProperty("TicketNumber", ticketPayment.TicketNumber))
             {
-                _logger.LogInformation("get create ticket payment request.");
-                var response = await _mediator.Send(ticketPayment);
-                return Ok(ApiResponse.Result(response));
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Create ticket payment request failed");
-                return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Message(e.Message));
+                try
+                {
+                    _logger.LogInformation("get create ticket payment request.");
+                    var response = await _mediator.Send(ticketPayment);
+                    return Ok(ApiResponse.Result(response));
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Create ticket payment request failed");
+                    return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Message(e.Message));
+                }
             }
         }
 
@@ -105,16 +115,19 @@ namespace Gov.CitizenApi.Features.Tickets
         [ProducesResponseType(typeof(ApiMessageResponse), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Pay([FromQuery] TicketPaymentConfirmCommand ticketPayConfirm)
         {
-            try
+            using (LogContext.PushProperty("TransactionId", ticketPayConfirm.TransactionId))
             {
-                _logger.LogInformation("get ticket payment confirmation");
-                var response = await _mediator.Send(ticketPayConfirm);
-                return RedirectToAction("Ticket", new { ticketNumber = response.TicketNumber, time = response.Time });
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Update ticket payment failed");
-                return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Message(e.Message));
+                try
+                {
+                    _logger.LogInformation("get ticket payment confirmation");
+                    var response = await _mediator.Send(ticketPayConfirm);
+                    return RedirectToAction("Ticket", new { ticketNumber = response.TicketNumber, time = response.Time });
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Update ticket payment failed");
+                    return StatusCode(StatusCodes.Status500InternalServerError, ApiResponse.Message(e.Message));
+                }
             }
         }
 
