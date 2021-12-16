92b60c65ddad55949be17e6696fdb28905703937
kanwalgupta <kanwal.gupta1@gmail.com>
2021-12-16 00:09:46 -0800

    Migrate tslint to eslint

--- src/frontend/citizen-portal/src/app/shared/models/disputantView.model.ts
+++ src/frontend/citizen-portal/src/app/shared/models/disputantView.model.ts
@@ -1,7 +1,7 @@
 import { Disputant } from 'app/api';
 import { Address } from './address.model';
 
-// tslint:disable-next-line
+// eslint-disable-next-line
 export interface DisputantView extends Disputant {
   // TODO remove once backend has added this to their side
   _mailingAddress: Address;
