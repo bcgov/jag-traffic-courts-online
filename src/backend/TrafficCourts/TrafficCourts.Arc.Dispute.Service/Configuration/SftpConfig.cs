f0472a4bcaf1225b7b6c946d3607f572efdb4158
Burak Karahan <burak.karahan@nttdata.com>
2022-03-07 11:18:02 -0500

    Added ssh key authentication option for sftp client config

--- src/backend/TrafficCourts/TrafficCourts.Arc.Dispute.Service/Configuration/SftpConfig.cs
+++ src/backend/TrafficCourts/TrafficCourts.Arc.Dispute.Service/Configuration/SftpConfig.cs
@@ -7,5 +7,6 @@
         public int Port { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
+        public string? SshPrivateKeyPath { get; set; }
     }
 }
