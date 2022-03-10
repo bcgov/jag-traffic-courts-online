697c993e3660958a1594d1f17cdeb0534dfc53cf
Burak Karahan <burak.karahan@nttdata.com>
2022-03-10 16:57:26 -0500

    Added CRUD operations for notice of dispute model utilizing Spring Data JPA and in memory H2 database

---
+++ src/backend/oracle-data-interface/src/main/java/ca/bc/gov/trafficcourtsonline/oracledatainterface/repository/DisputeRepository.java
@@ -0,0 +1,9 @@
+package ca.bc.gov.trafficcourtsonline.oracledatainterface.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.bc.gov.trafficcourtsonline.oracledatainterface.model.Dispute;
+
+public interface DisputeRepository extends CrudRepository<Dispute, Integer>{
+
+}
