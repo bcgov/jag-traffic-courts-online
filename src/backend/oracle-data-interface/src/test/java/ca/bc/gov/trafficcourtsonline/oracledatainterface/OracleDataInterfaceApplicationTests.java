92e729b6115c2320b90d24f807b5e832c4996599
Burak Karahan <burak.karahan@nttdata.com>
2022-03-10 15:06:27 -0500

    Created Oracle Data Interface Spring Boot app with basic configuration and dependencies

---
+++ src/backend/oracle-data-interface/src/test/java/ca/bc/gov/trafficcourtsonline/oracledatainterface/OracleDataInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.bc.gov.trafficcourtsonline.oracledatainterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleDataInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
