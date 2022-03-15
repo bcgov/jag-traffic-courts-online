ecfff4e58a457a79f53b24a6acd43e4bd6027c2d
Burak Karahan <burak.karahan@nttdata.com>
2022-03-15 15:09:26 -0400

    Refactored test folder's package and class naming path to be consistent with java main path

--- src/backend/oracle-data-interface/src/test/java/ca/bc/gov/open/jag/tco/oracledatainterface/OracleDataInterfaceApplicationTests.java
+++ src/backend/oracle-data-interface/src/test/java/ca/bc/gov/open/jag/tco/oracledatainterface/OracleDataInterfaceApplicationTests.java
@@ -1,4 +1,4 @@
-package ca.bc.gov.trafficcourtsonline.oracledatainterface;
+package ca.bc.gov.open.jag.tco.oracledatainterface;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
