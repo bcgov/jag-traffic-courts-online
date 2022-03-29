141921d16daf8c85716c7987f0a5cfc922de8df4
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2022-03-29 13:10:40 -0700

    TCVP-932 updated ocr validation rules, refactor per naming conventions
    updated unit tests

--- src/backend/TrafficCourts/Citizen.Service/Validators/Rules/LowConfidenceGlobalRule.cs
+++ src/backend/TrafficCourts/Citizen.Service/Validators/Rules/LowConfidenceGlobalRule.cs
@@ -5,29 +5,40 @@ namespace TrafficCourts.Citizen.Service.Validators.Rules;
 
 public class LowConfidenceGlobalRule
 {
-    private static readonly float MinViableConfidence = 0.8f;
+    private static readonly float _minViableConfidence = 0.8f;
 
     public static void Run(OcrViolationTicket violationTicket)
     {
-        // If any 3 out of the below fields have a low confidence, reject the entire ticket
+        // If any {minViableThreshold} out of the below fields have a low confidence, reject the entire ticket,
+        // where {minViableThreshold} is 4, 6, or 8 for 1 Count, 2 Counts, 3 Counts populated respectively.
         int numOfLowConfFields = 0;
-        numOfLowConfFields += (violationTicket.Fields[Surname].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[GivenName].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[DriverLicenceNumber].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[DriverLicenceProvince].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count1Description].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count1ActRegs].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count1Section].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count1TicketAmount].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count2Description].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count2ActRegs].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count2Section].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count2TicketAmount].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count3Description].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count3ActRegs].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count3Section].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        numOfLowConfFields += (violationTicket.Fields[Count3TicketAmount].FieldConfidence < MinViableConfidence) ? 1 : 0;
-        if (numOfLowConfFields >= 3) {
+        int minViableThreshold = 4;
+        numOfLowConfFields += (violationTicket.Fields[Surname].FieldConfidence < _minViableConfidence) ? 1 : 0;
+        numOfLowConfFields += (violationTicket.Fields[GivenName].FieldConfidence < _minViableConfidence) ? 1 : 0;
+        numOfLowConfFields += (violationTicket.Fields[DriverLicenceNumber].FieldConfidence < _minViableConfidence) ? 1 : 0;
+        numOfLowConfFields += (violationTicket.Fields[DriverLicenceProvince].FieldConfidence < _minViableConfidence) ? 1 : 0;
+        numOfLowConfFields += (violationTicket.Fields[Count1Description].FieldConfidence < _minViableConfidence) ? 1 : 0;
+        numOfLowConfFields += (violationTicket.Fields[Count1ActRegs].FieldConfidence < _minViableConfidence) ? 1 : 0;
+        numOfLowConfFields += (violationTicket.Fields[Count1Section].FieldConfidence < _minViableConfidence) ? 1 : 0;
+        numOfLowConfFields += (violationTicket.Fields[Count1TicketAmount].FieldConfidence < _minViableConfidence) ? 1 : 0;
+        if (violationTicket.IsCount2Populated())
+        {
+            numOfLowConfFields += (violationTicket.Fields[Count2Description].FieldConfidence < _minViableConfidence) ? 1 : 0;
+            numOfLowConfFields += (violationTicket.Fields[Count2ActRegs].FieldConfidence < _minViableConfidence) ? 1 : 0;
+            numOfLowConfFields += (violationTicket.Fields[Count2Section].FieldConfidence < _minViableConfidence) ? 1 : 0;
+            numOfLowConfFields += (violationTicket.Fields[Count2TicketAmount].FieldConfidence < _minViableConfidence) ? 1 : 0;
+            minViableThreshold += 2;
+        }
+        if (violationTicket.IsCount3Populated())
+        {
+            numOfLowConfFields += (violationTicket.Fields[Count3Description].FieldConfidence < _minViableConfidence) ? 1 : 0;
+            numOfLowConfFields += (violationTicket.Fields[Count3ActRegs].FieldConfidence < _minViableConfidence) ? 1 : 0;
+            numOfLowConfFields += (violationTicket.Fields[Count3Section].FieldConfidence < _minViableConfidence) ? 1 : 0;
+            numOfLowConfFields += (violationTicket.Fields[Count3TicketAmount].FieldConfidence < _minViableConfidence) ? 1 : 0;
+            minViableThreshold += 2;
+        }
+        if (numOfLowConfFields >= minViableThreshold)
+        {
             violationTicket.GlobalValidationErrors.Add(ValidationMessages.LowConfidenceError);
         }
     }
