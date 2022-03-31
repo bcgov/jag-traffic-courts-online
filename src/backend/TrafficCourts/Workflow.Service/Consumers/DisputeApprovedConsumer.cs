c105565d89770674df09846fa613e4b063464655
Burak Karahan <burak.karahan@nttdata.com>
2022-03-31 10:16:26 -0400

    Added a service to send dispute ticket data to ARC API via httpclient

--- src/backend/TrafficCourts/Workflow.Service/Consumers/DisputeApprovedConsumer.cs
+++ src/backend/TrafficCourts/Workflow.Service/Consumers/DisputeApprovedConsumer.cs
@@ -20,70 +20,77 @@ namespace TrafficCourts.Workflow.Service.Consumers
         }
         public async Task Consume(ConsumeContext<DisputeApproved> context)
         {
-            if (context.RequestId != null)
+            try
             {
-                _logger.LogInformation("DisputeApprovedConsumer is consuming message: " + context.Message.Id);
-
-                List<Models.TicketDetails> ticketDetails = new List<Models.TicketDetails>();
-
-                foreach (var td in context.Message.TicketDetails)
+                if (context.RequestId != null)
                 {
-                    var ticketDetail = new Models.TicketDetails
+                    _logger.LogInformation("DisputeApprovedConsumer is consuming message: " + context.Message.Id);
+
+                    List<Models.TicketDetails> ticketDetails = new List<Models.TicketDetails>();
+
+                    foreach (var td in context.Message.TicketDetails)
                     {
-                        Section = td.Section,
-                        Subsection = td.Subsection,
-                        Paragraph = td.Paragraph,
-                        Act = td.Act,
-                        Amount = td.Amount
-                    };
-                    ticketDetails.Add(ticketDetail);
-                }
-
-                // Add dispute details model as part of TcoDisputeTicket model if defined in the message
-                Dictionary<string, Models.DisputeDetails>[] disputeDetailsArray = new Dictionary<string, Models.DisputeDetails>[3];
-
-                if (context.Message.DisputeDetails != null && context.Message.DisputeDetails.Length > 0 && context.Message.DisputeDetails.Length < 4)
-                {
-                    for (int i = 0; i < context.Message.DisputeDetails.Length; i++)
-                    {
-                        Dictionary<string, Models.DisputeDetails> disputeDetailsDictionary = new Dictionary<string, Models.DisputeDetails>();
-
-                        foreach (KeyValuePair<string, Messaging.MessageContracts.DisputeDetails> kvp in context.Message.DisputeDetails[i])
+                        var ticketDetail = new Models.TicketDetails
                         {
-                            Models.DisputeDetails disputeDetail = new Models.DisputeDetails
-                            {
-                                DisputeType = kvp.Value.DisputeType,
-                                DisputeReason = kvp.Value.DisputeReason
-                            };
-
-                            disputeDetailsDictionary.Add(kvp.Key, disputeDetail);
-                        }
-
-                        disputeDetailsArray[i] = disputeDetailsDictionary;
+                            Section = td.Section,
+                            Subsection = td.Subsection,
+                            Paragraph = td.Paragraph,
+                            Act = td.Act,
+                            Amount = td.Amount
+                        };
+                        ticketDetails.Add(ticketDetail);
                     }
+
+                    // Add dispute details model as part of TcoDisputeTicket model if defined in the message
+                    Dictionary<string, Models.DisputeDetails>[] disputeDetailsArray = new Dictionary<string, Models.DisputeDetails>[3];
+
+                    if (context.Message.DisputeDetails != null && context.Message.DisputeDetails.Length > 0 && context.Message.DisputeDetails.Length < 4)
+                    {
+                        for (int i = 0; i < context.Message.DisputeDetails.Length; i++)
+                        {
+                            Dictionary<string, Models.DisputeDetails> disputeDetailsDictionary = new Dictionary<string, Models.DisputeDetails>();
+
+                            foreach (KeyValuePair<string, Messaging.MessageContracts.DisputeDetails> kvp in context.Message.DisputeDetails[i])
+                            {
+                                Models.DisputeDetails disputeDetail = new Models.DisputeDetails
+                                {
+                                    DisputeType = kvp.Value.DisputeType,
+                                    DisputeReason = kvp.Value.DisputeReason
+                                };
+
+                                disputeDetailsDictionary.Add(kvp.Key, disputeDetail);
+                            }
+
+                            disputeDetailsArray[i] = disputeDetailsDictionary;
+                        }
+                    }
+
+                    var tcoDisputeTicket = new TcoDisputeTicket
+                    {
+                        CitizenName = context.Message.CitizenName,
+                        TicketIssuanceDate = context.Message.TicketIssuanceDate,
+                        TicketFileNumber = context.Message.TicketFileNumber,
+                        IssuingOrganization = context.Message.IssuingOrganization,
+                        IssuingLocation = context.Message.IssuingLocation,
+                        DriversLicense = context.Message.DriversLicense,
+                        TicketDetails = ticketDetails,
+                        StreetAddress = context.Message.StreetAddress,
+                        City = context.Message.City,
+                        Province = context.Message.Province,
+                        PostalCode = context.Message.PostalCode,
+                        Email = context.Message.Email,
+                        DisputeDetails = disputeDetailsArray
+                    };
+
+                    _logger.LogDebug("TRY SENDING APPROVED DISPUTE TO ARC: " + tcoDisputeTicket.ToString());
+
+                    await _submitDisputeToArcService.SubmitDisputeToArcAsync(tcoDisputeTicket);
+
                 }
-
-                var tcoDisputeTicket = new TcoDisputeTicket
-                {
-                    CitizenName = context.Message.CitizenName,
-                    TicketIssuanceDate = context.Message.TicketIssuanceDate,
-                    TicketFileNumber = context.Message.TicketFileNumber,
-                    IssuingOrganization = context.Message.IssuingOrganization,
-                    IssuingLocation = context.Message.IssuingLocation,
-                    DriversLicense = context.Message.DriversLicense,
-                    TicketDetails = ticketDetails,
-                    StreetAddress = context.Message.StreetAddress,
-                    City = context.Message.City,
-                    Province = context.Message.Province,
-                    PostalCode = context.Message.PostalCode,
-                    Email = context.Message.Email,
-                    DisputeDetails = disputeDetailsArray
-                };
-
-                _logger.LogInformation("TRY SENDING APPROVED DISPUTE TO ARC: " + tcoDisputeTicket.ToString());
-
-                await _submitDisputeToArcService.SubmitDisputeToArcAsync(tcoDisputeTicket);
-
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("DisputeApprovedConsumer", ex);
             }
         }
     }
--- src/backend/TrafficCourts/Workflow.Service/Services/SubmitDisputeToArcService.cs
+++ src/backend/TrafficCourts/Workflow.Service/Services/SubmitDisputeToArcService.cs
@@ -1,12 +1,41 @@
-﻿using TrafficCourts.Workflow.Service.Models;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using TrafficCourts.Workflow.Service.Models;
 
 namespace TrafficCourts.Workflow.Service.Services
 {
     public class SubmitDisputeToArcService : ISubmitDisputeToArcService
     {
-        public Task SubmitDisputeToArcAsync(TcoDisputeTicket approvedDispute)
+        private readonly ILogger<SubmitDisputeToArcService> _logger;
+
+        public SubmitDisputeToArcService(ILogger<SubmitDisputeToArcService> logger)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+        }
+
+        public async Task SubmitDisputeToArcAsync(TcoDisputeTicket approvedDispute)
+        {
+            // Formatting all TcoDisputeTicket class properties to camel case since ARC api accepts kebab case only after serialization
+            var formatter = new JsonMediaTypeFormatter();
+            formatter.SerializerSettings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new DefaultContractResolver { NamingStrategy = new KebabCaseNamingStrategy() }
+            };
+
+            using var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(@"https://localhost:7082/");
+
+            using var response = await httpClient.PostAsync("api/TcoDisputeTicket", approvedDispute, formatter);
+            if (response.IsSuccessStatusCode)
+            {
+                var apiResponse = await response.Content.ReadAsStringAsync();
+                if (apiResponse != null)
+                {
+                    _logger.LogInformation("An ARC file has been created successfully with the following data: " + apiResponse);
+                }
+            }
         }
     }
 }
