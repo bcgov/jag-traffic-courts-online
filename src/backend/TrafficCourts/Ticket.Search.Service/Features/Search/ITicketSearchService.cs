04d60e0a307b95a9a1437f26dc5013b7b3de15a5
Phil Bolduc <philbolduc@gmail.com>
2022-04-05 16:15:11 -0700

    Remove obsolete Ticket.Search.Service

--- src/backend/TrafficCourts/Ticket.Search.Service/Features/Search/ITicketSearchService.cs
+++
@@ -1,9 +0,0 @@
-﻿using TrafficCourts.Ticket.Search.Service.Features.Search.RoadSafetyTicketSearch;
-
-namespace TrafficCourts.Ticket.Search.Service.Features.Search
-{
-    public interface ITicketSearchService
-    {
-        Task<IEnumerable<Invoice>> SearchTicketAsync(string ticketNumber, string time, CancellationToken cancellationToken);
-    }
-}
