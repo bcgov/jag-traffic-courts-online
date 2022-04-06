670bc2d2b76e9748ef77a4ce7f801d07333d6521
Phil Bolduc <philbolduc@gmail.com>
2022-04-05 20:07:59 -0700

    Add unit tests for Common library

--- src/backend/TrafficCourts/Common/Configuration/TrafficCourtsConfiguration.cs
+++ src/backend/TrafficCourts/Common/Configuration/TrafficCourtsConfiguration.cs
@@ -1,7 +1,8 @@
-﻿using Serilog.Events;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace TrafficCourts.Common.Configuration;
 
+[ExcludeFromCodeCoverage]
 public abstract class TrafficCourtsConfiguration
 {
 }
---
+++ src/backend/TrafficCourts/TrafficCourts.Common.Test/Configuration/ConfigurationTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TrafficCourts.Common.Configuration;
+using Xunit;
+
+namespace TrafficCourts.Common.Test.Configuration;
+
+public class ConfigurationTest
+{
+    [Fact]
+    public void ConfigureJsonOptions_should_add_JsonStringEnumConverter()
+    {
+        var options = new Microsoft.AspNetCore.Mvc.JsonOptions();
+        Assert.Equal(0, options.JsonSerializerOptions.Converters.Count); // precondition check
+
+        ConfigureJsonOptions sut = new ConfigureJsonOptions();
+        
+        sut.Configure(options);
+
+        Assert.Equal(1, options.JsonSerializerOptions.Converters.Count);
+        Assert.IsType<JsonStringEnumConverter>(options.JsonSerializerOptions.Converters[0]);
+    }
+}
