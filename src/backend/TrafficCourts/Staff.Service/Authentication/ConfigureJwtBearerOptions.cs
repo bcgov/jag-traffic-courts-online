a5e03eea78127ab0b0ec08fcd9630884164efa04
Phil Bolduc <philbolduc@gmail.com>
2022-04-08 15:38:37 -0700

    TCVP-1233 - add Staff API project

---
+++ src/backend/TrafficCourts/Staff.Service/Authentication/ConfigureJwtBearerOptions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+
+namespace TrafficCourts.Staff.Service.Authentication;
+
+public class ConfigureJwtBearerOptions : IConfigureOptions<JwtBearerOptions>
+{
+    private readonly IConfiguration _configuration;
+
+    public ConfigureJwtBearerOptions(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    }
+
+    public void Configure(JwtBearerOptions options)
+    {
+        options.Authority = _configuration["Jwt:Authority"];
+        options.Audience = _configuration["Jwt:Audience"];
+    }
+}
---
+++ src/backend/TrafficCourts/Staff.Service/Logging/Diagnostics.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics;
+
+namespace TrafficCourts.Staff.Service.Logging;
+
+public static class Diagnostics
+{
+    public const string ServiceName = "staff-api";
+    public static readonly ActivitySource Source = new ActivitySource(ServiceName);
+}
