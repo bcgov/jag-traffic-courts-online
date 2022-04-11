a7e5396be2a8f8852bc10aa0668ae869410d787a
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2022-04-11 12:00:16 -0700

    Added OrafaceAPI as an OpenAPI connected service

---
+++ src/backend/TrafficCourts/Staff.Service/OpenAPIs/OracleDataAPI/v1_0/IOracleDataApi_v1_0Client.cs
@@ -0,0 +1,14 @@
+﻿namespace TrafficCourts.Staff.Service.OpenAPIs.OracleDataApi.v1_0;
+
+/// <summary>
+/// Generated/Extracted from OracleDataApi_v1_0Client, partial extraction (only public methods with CancellationToken as a param)
+/// </summary>
+public interface IOracleDataApi_v1_0Client
+{
+    Task CodeTableRefreshAsync(CancellationToken cancellationToken);
+    Task DeleteDisputeAsync(int disputeId, CancellationToken cancellationToken);
+    Task<ICollection<Dispute>> GetAllDisputesAsync(CancellationToken cancellationToken);
+    Task<Dispute> GetDisputeAsync(int disputeId, CancellationToken cancellationToken);
+    Task<int> SaveDisputeAsync(Dispute body, CancellationToken cancellationToken);
+    Task<Dispute> UpdateAsync(Dispute body, CancellationToken cancellationToken);
+}
---
+++ src/backend/TrafficCourts/TrafficCourts.Staff.Service.Test/Controllers/DisputeControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading;
+using TrafficCourts.Staff.Service.Controllers;
+using TrafficCourts.Staff.Service.OpenAPIs.OracleDataApi.v1_0;
+using Xunit;
+
+namespace TrafficCourts.Staff.Service.Test.Controllers;
+
+public class DisputeControllerTest
+{
+
+    [Fact]
+    public async void TestGetDisputeOkResult()
+    {
+        // Mock the OracleDataApi to return a specific Dispute for (1), confirm controller returns the dispute.
+
+        // Arrange
+        Dispute dispute = new();
+        dispute.Id = 1;
+        var oracleDataApi = new Mock<IOracleDataApi_v1_0Client>();
+        oracleDataApi
+            .Setup(_ => _.GetDisputeAsync(It.Is<int>(v => v == 1), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(dispute);
+        var mockLogger = new Mock<ILogger<DisputeController>>();
+        DisputeController disputeController = new (oracleDataApi.Object, mockLogger.Object);
+
+        // Act
+        IActionResult? result = await disputeController.GetDisputeAsync(1, CancellationToken.None);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result);
+        Assert.Equal(dispute, okResult.Value);
+    }
+}
