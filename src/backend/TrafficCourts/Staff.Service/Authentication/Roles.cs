27fbcc3653b2179a482ab679ad315e6cd2a0027e
Phil Bolduc <philbolduc@gmail.com>
2022-04-11 10:42:34 -0700

    TCVP-1246 add authorization to api, swagger

---
+++ src/backend/TrafficCourts/Staff.Service/Authentication/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace TrafficCourts.Staff.Service.Authentication;
+
+public static class Roles
+{
+    /// <summary>
+    /// Violation Ticket Centre Staff API User
+    /// </summary>
+    public const string User = "vtc-user";
+}
