d02f2c9d3a70d9b6bac24f978631b9c993abd1fa
EWong <elainecwong@outlook.com>
2022-04-20 15:50:39 -0700

    Fixed strings to remove the $ prepended.

--- src/backend/TrafficCourts/Workflow.Service/Services/EmailSenderService.cs
+++ src/backend/TrafficCourts/Workflow.Service/Services/EmailSenderService.cs
@@ -52,12 +52,12 @@ namespace TrafficCourts.Workflow.Service.Services
                 if (String.IsNullOrEmpty(emailMessage.Subject) || email.Body is null)
                 {
                     _logger.LogError("No subject or message body provided.");
-                    throw new InvalidEmailMessageException($"No subject or message body provided");
+                    throw new InvalidEmailMessageException("No subject or message body provided");
                 }
                 else if (email.To.Count == 0 && email.Cc.Count == 0 && email.Bcc.Count == 0)
                 {
                     _logger.LogError("Missing recipient info.  No To, Cc or Bcc provided.");
-                    throw new InvalidEmailMessageException($"Missing recipient info");
+                    throw new InvalidEmailMessageException("Missing recipient info");
                 }
 
                 // send email asynchronously
@@ -69,49 +69,49 @@ namespace TrafficCourts.Workflow.Service.Services
             {
                 // host or message is null.
                 _logger.LogError(ane, "Host or message is null.");
-                throw new EmailSendFailedException($"Host or message is null", ane);
+                throw new EmailSendFailedException("Host or message is null", ane);
             }
             catch (ObjectDisposedException ode)
             {
                 // The MailKit.MailTransport has been disposed.
                 _logger.LogError(ode, "MailTransport has been disposed.");
-                throw new EmailSendFailedException($"MailTransport has been disposed", ode);
+                throw new EmailSendFailedException("MailTransport has been disposed", ode);
             }
             catch (MailKit.ServiceNotConnectedException snce)
             {
                 // The MailKit.MailTransport is not connected.
                 _logger.LogError(snce, "MailTransport is not connected.");
-                throw new EmailSendFailedException($"MailTransport is not connected", snce);
+                throw new EmailSendFailedException("MailTransport is not connected", snce);
             }
             catch (MailKit.ServiceNotAuthenticatedException snae)
             {
                 // Authentication is required before sending a message.
                 _logger.LogError(snae, "Authentication is required before sending a message.");
-                throw new EmailSendFailedException($"Authentication is required before sending a message", snae);
+                throw new EmailSendFailedException("Authentication is required before sending a message", snae);
             }
             catch (OperationCanceledException oce)
             {
                 // The operation was canceled. (cancellationToken was called)
                 _logger.LogError(oce, "The operation was canceled.");
-                throw new EmailSendFailedException($"The operation was canceled", oce);
+                throw new EmailSendFailedException("The operation was canceled", oce);
             }
             catch (System.IO.IOException ioe)
             {
                 // An I/O error occurred.
                 _logger.LogError(ioe, "An I/O error occurred.");
-                throw new EmailSendFailedException($"An I/O error occurred", ioe);
+                throw new EmailSendFailedException("An I/O error occurred", ioe);
             }
             catch (MailKit.CommandException ce)
             {
                 // The send command failed.
                 _logger.LogError(ce, "The send command failed.");
-                throw new EmailSendFailedException($"The send command failed", ce);
+                throw new EmailSendFailedException("The send command failed", ce);
             }
             catch (MailKit.ProtocolException pe)
             {
                 // A protocol exception occurred.
                 _logger.LogError(pe, "A protocol exception occurred.");
-                throw new EmailSendFailedException($"Protocol exception", pe);
+                throw new EmailSendFailedException("Protocol exception", pe);
             }
             catch (InvalidOperationException ioe)
             {
@@ -119,13 +119,13 @@ namespace TrafficCourts.Workflow.Service.Services
                 // -or-
                 // No recipients have been specified.
                 _logger.LogError(ioe, "A sender has not been specified.");
-                throw new EmailSendFailedException($"Possible missing sender info", ioe);
+                throw new EmailSendFailedException("Possible missing sender info", ioe);
             }
             catch (SmtpConnectFailedException scfe)
             {
                 // An error connecting to the the SMTP Server
                 _logger.LogError(scfe, "An error connecting to the the SMTP Server.");
-                throw new EmailSendFailedException($"An error connecting to the the SMTP Server", scfe);
+                throw new EmailSendFailedException("An error connecting to the the SMTP Server", scfe);
             }
         }
 
