ab5bd4beaaffbc91ddac6486dd636097c9f40995
Phil Bolduc <philbolduc@gmail.com>
2022-04-23 19:30:32 -0700

    Add documentation comments, move class to own file

--- src/backend/TrafficCourts/Citizen.Service/Services/Tickets/Search/ITicketInvoiceSearchService.cs
+++ src/backend/TrafficCourts/Citizen.Service/Services/Tickets/Search/ITicketInvoiceSearchService.cs
@@ -7,5 +7,12 @@ namespace TrafficCourts.Citizen.Service.Services.Tickets.Search;
 /// </summary>
 public interface ITicketInvoiceSearchService
 {
+    /// <summary>
+    /// Searches for a violation ticket invoices (counts) with the matching ticket number and issue time.
+    /// </summary>
+    /// <param name="ticketNumber">The violation ticket number.</param>
+    /// <param name="issuedTime">The time the violation ticket was issued at.</param>
+    /// <param name="cancellationToken">The cancellation token to cancel operation.</param>
+    /// <returns>The matching violation ticket invoices (counts).</returns>
     Task<IEnumerable<Invoice>> SearchAsync(string ticketNumber, TimeOnly issuedTime, CancellationToken cancellationToken);
 }
---
+++ src/backend/TrafficCourts/Citizen.Service/Services/Tickets/Search/TicketSearchErrorException.cs
@@ -0,0 +1,10 @@
+﻿namespace TrafficCourts.Citizen.Service.Services.Tickets.Search;
+
+[Serializable]
+public class TicketSearchErrorException : Exception
+{
+    public TicketSearchErrorException(string message, Exception inner) : base(message, inner) { }
+    protected TicketSearchErrorException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}
