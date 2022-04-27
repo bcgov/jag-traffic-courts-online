411c5d2bbebbc7712a5c8256ceee48e29b6c1239
Lorraine Dame <Lorraine.Dame@nttdata.com>
2022-04-27 11:19:29 -0700

    updates re pr comments

--- src/frontend/citizen-portal/src/app/components/shell-ticket/shell-ticket.component.ts
+++ src/frontend/citizen-portal/src/app/components/shell-ticket/shell-ticket.component.ts
@@ -676,7 +676,7 @@ const data2: TicketDisputeView
       violationTime: invoiceTimeField.value
         ? invoiceTimeField.value.replace(' ', ':')
         : '',
-      violationDate: new Date(invoiceDateField.value),
+        violationDate: new Date(moment(invoiceDateField.value).utc().format('L')),
 
       lastName: surnameField.value
         ? surnameField.value
