b80f755f0b3e041305ad3e8827fb4e8b06e52c04
Lorraine Dame <Lorraine.Dame@gov.bc.ca>
2022-04-28 09:20:26 -0700

    no blank line

--- src/frontend/staff-portal/src/app/services/date.service.ts
+++ src/frontend/staff-portal/src/app/services/date.service.ts
@@ -3,10 +3,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'dateSuffix' })
 export class DateSuffixPipe implements PipeTransform {
     transform(value: string): string {
-
     let suffix = 'th',
         day = value;
-
         if (day === '1' || day === '21' || day === '31') {
             suffix = 'st'
         } else if (day === '2' || day === '22') {
@@ -14,8 +12,6 @@ export class DateSuffixPipe implements PipeTransform {
         } else if (day === '3' || day === '23') {
            suffix = 'rd';
         }
-
         return suffix;
-
     }
 }
