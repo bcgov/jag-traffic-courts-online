fb4db20ee6abca91a9bfe5344bfad479a6080a1d
Sharkww <39256528+Sharkww@users.noreply.github.com>
2022-04-28 08:59:06 -0700

    Move api generate cmd to package.json

--- src/frontend/citizen-portal/src/app/api/generate.sh
+++
@@ -1,3 +0,0 @@
-docker pull openapitools/openapi-generator-cli:latest
-
-docker run --rm -v "$(pwd)":/local openapitools/openapi-generator-cli generate -i /local/client-api.json -g typescript-angular -t /local/templates --additional-properties=modelFileSuffix=.model -o /local
