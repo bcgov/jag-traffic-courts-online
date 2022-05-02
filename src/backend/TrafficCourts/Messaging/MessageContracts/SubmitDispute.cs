7205a7d2d4eac28fe86b5b69aa168ee59417ebd9
Burak Karahan <burak.karahan@nttdata.com>
2022-05-02 16:30:06 -0400

    Converted submit dispute message contract interfaces to classes

--- src/backend/TrafficCourts/Messaging/MessageContracts/SubmitDispute.cs
+++ src/backend/TrafficCourts/Messaging/MessageContracts/SubmitDispute.cs
@@ -3,30 +3,30 @@ using TrafficCourts.Common.Converters;
 
 namespace TrafficCourts.Messaging.MessageContracts
 {
-    public interface SubmitDispute : IMessage
+    public class SubmitDispute : IMessage
     {
-        string TicketNumber { get; set; }
-        string CourtLocation { get; set; }
-        DateTime ViolationDate { get; set; }
-        string DisputantSurname { get; set; }
-        string GivenNames { get; set; }
-        string StreetAddress { get; set; }
-        string Province { get; set; }
-        string PostalCode { get; set; }
-        string HomePhone { get; set; }
-        string EmailAddress { get; set; }
-        string DriversLicence { get; set; }
-        string DriversLicenceProvince { get; set; }
-        string WorkPhone { get; set; }
+        public string TicketNumber { get; set; }
+        public string CourtLocation { get; set; }
+        public DateTime ViolationDate { get; set; }
+        public string DisputantSurname { get; set; }
+        public string GivenNames { get; set; }
+        public string StreetAddress { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string HomePhone { get; set; }
+        public string EmailAddress { get; set; }
+        public string DriversLicence { get; set; }
+        public string DriversLicenceProvince { get; set; }
+        public string WorkPhone { get; set; }
         [JsonConverter(typeof(DateOnlyJsonConverter))]
-        DateOnly DateOfBirth { get; set; }
-        string EnforcementOrganization { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string EnforcementOrganization { get; set; }
         [JsonConverter(typeof(DateOnlyJsonConverter))]
-        DateOnly ServiceDate { get; set; }
-        IList<TicketCount> TicketCounts { get; set; }
-        bool LawyerRepresentation { get; set; }
-        string InterpreterLanguage { get; set; }
-        bool WitnessIntent { get; set; }
-        string OcrViolationTicket { get; set; }
+        public DateOnly ServiceDate { get; set; }
+        public IList<TicketCount> TicketCounts { get; set; }
+        public bool LawyerRepresentation { get; set; }
+        public string InterpreterLanguage { get; set; }
+        public bool WitnessIntent { get; set; }
+        public string OcrViolationTicket { get; set; }
     }
 }
