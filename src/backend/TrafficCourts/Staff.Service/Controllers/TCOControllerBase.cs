943e6d724d1c24d6071be596f73759c7b373cda0
Lorraine Dame <Lorraine.Dame@gov.bc.ca>
2022-05-03 16:37:29 -0700

    unauthorized for docker version of api

---
+++ src/backend/TrafficCourts/Staff.Service/Controllers/TCOControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TrafficCourts.Staff.Service.Authentication;
+
+namespace TrafficCourts.Staff.Service.Controllers
+{
+    [ApiController]
+    [Authorize(Roles = Roles.User)]
+    [Route("api/[controller]")]
+    public abstract class TCOControllerBase<TController> : ControllerBase
+    {
+        protected readonly ILogger<TController> _logger;
+
+        protected TCOControllerBase(ILogger<TController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
