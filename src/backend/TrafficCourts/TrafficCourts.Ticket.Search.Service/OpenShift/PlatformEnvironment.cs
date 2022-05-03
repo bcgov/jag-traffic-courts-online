d6406e365e98a07d7c5ad74dc7beb4699d87784f
Phil Bolduc <philbolduc@gmail.com>
2022-05-03 09:08:34 -0700

    Remove ticket search service

--- src/backend/TrafficCourts/TrafficCourts.Ticket.Search.Service/OpenShift/PlatformEnvironment.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TrafficCourts.Ticket.Search.Service.OpenShift
-{
-    public static class PlatformEnvironment
-    {
-        public static bool IsOpenShift => OpenShiftEnvironment.IsOpenShift;
-    }
-}
