3ca3fa32d29e2e09bb44d468c8e23ad6b716c657
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2022-05-17 14:57:31 -0700

    TCVP-933 Added new ocr validation rule
    All 3 counts must now have the text "MVA" and nothing else.

---
+++ src/backend/TrafficCourts/Citizen.Service/Validators/Rules/CountActRegMustBeMVA.cs
@@ -0,0 +1,26 @@
+using TrafficCourts.Citizen.Service.Models.Tickets;
+
+namespace TrafficCourts.Citizen.Service.Validators.Rules;
+
+/// <summary>
+/// For all 3 counts, the ACT/REGs section must be "MVA" or blank.
+/// </summary>
+public class CountActRegMustBeMVA : ValidationRule
+{
+
+    private int _countNum;
+
+    public CountActRegMustBeMVA(Field field, int countNum) : base(field)
+    {
+        _countNum = countNum;
+    }
+
+    public override void Run()
+    {
+        string? countAct = this.Field.Value;
+        if (countAct is not null && !"MVA".Equals(countAct.Replace("\\s+", "").ToUpper()))
+        {
+            AddValidationError(String.Format(ValidationMessages.MVAMustBeCountValue, countAct, _countNum));
+        }
+    }
+}
