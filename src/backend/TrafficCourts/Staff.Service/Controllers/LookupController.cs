3f2cff8dea9161533e8c6b3004d443085470920f
Lorraine Dame <Lorraine.Dame@gov.bc.ca>
2022-05-19 09:29:54 -0700

    fix dependencies when using seq or splunk logging, add examples to README.md

---
+++ src/backend/TrafficCourts/Staff.Service/Controllers/LookupController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Statute = TrafficCourts.Staff.Service.Models.Statute;
+using TrafficCourts.Staff.Service.OpenAPIs.OracleDataApi.v1_0;
+using TrafficCourts.Staff.Service.Services;
+
+namespace TrafficCourts.Staff.Service.Controllers;
+
+public class LookupController : TCOControllerBase<LookupController>
+{
+    private readonly ILookupService _lookupService;
+    
+    public LookupController(ILookupService lookupService, ILogger<LookupController> logger) : base(logger)
+    {
+        _lookupService = lookupService ?? throw new ArgumentNullException(nameof(lookupService));
+    }
+
+    /// <summary> 
+    /// Returns a list of Violation Ticket Statutes filtered by given section text (if provided).
+    /// </summary>
+    /// <param name="section">Motor vehicle act Section text to query by, ie "13(1)(a)" returns "Motor Vehicle or Trailer without Licence" contravention, or blank for no filter.</param>
+    /// <returns></returns>
+    [HttpGet]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(IList<Statute>), (int)HttpStatusCode.OK)]
+    public IActionResult Statutes(string? section)
+    {
+        _logger.LogDebug("Retrieving a Statutes");
+
+        List<Statute> statutes = _lookupService.GetStatutes(section).ToList();
+
+        return Ok(statutes);
+    }
+}
+
