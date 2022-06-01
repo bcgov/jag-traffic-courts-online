9de4fb7af4a323299ebba2f0ca32dd9512157ea2
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2022-06-01 09:05:18 -0700

    starting...

--- src/frontend/staff-portal/src/app/components/ticket-request/ticket-request.component.ts
+++ src/frontend/staff-portal/src/app/components/ticket-request/ticket-request.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Dispute } from 'app/api';
 
 @Component({
   selector: 'app-ticket-request',
@@ -6,15 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ticket-request.component.scss']
 })
 export class TicketRequestComponent implements OnInit {
+  @Input() disputeInfo: Dispute;
   public collapseObj: any = {
     contactInformation: true
- }
-  constructor() { }
+  }
+  public form: FormGroup;
+
+  constructor(
+    protected formBuilder: FormBuilder,
+  ) {
+    this.form = this.formBuilder.group({
+      timeToPayReason: null,
+      fineReductionReason: null
+    });
+    this.form.patchValue(this.disputeInfo);
+  }
 
   ngOnInit(): void {
   }
   public handleCollapse(name: string) {
-    this.collapseObj[name]= !this.collapseObj[name]
+    this.collapseObj[name] = !this.collapseObj[name]
   }
 
 }
