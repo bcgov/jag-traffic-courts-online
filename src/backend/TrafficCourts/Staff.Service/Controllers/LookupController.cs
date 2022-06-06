75833434bce1ec2e4c511b8f722e5d9aae76cbe9
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2022-06-06 10:34:44 -0700

    mergin

--- src/backend/TrafficCourts/Staff.Service/Controllers/LookupController.cs
+++ src/backend/TrafficCourts/Staff.Service/Controllers/LookupController.cs
@@ -10,19 +10,21 @@ namespace TrafficCourts.Staff.Service.Controllers;
 public class LookupController : ControllerBase
 {
     private readonly IMediator _mediator;
-    
-    public LookupController(IMediator mediator, ILogger<LookupController> logger) : base(logger)
+    private readonly ILogger<LookupController> _logger;
+
+    public LookupController(IMediator mediator, ILogger<LookupController> logger)
     {
         _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
-    /// <summary> 
-    /// Returns a list of Violation Ticket Statutes filtered by given section text (if provided).
-    /// </summary>
-    /// <param name="section">Motor vehicle act Section text to query by, ie "13(1)(a)" returns "Motor Vehicle or Trailer without Licence" contravention, or blank for no filter.</param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    [HttpGet]
+/// <summary> 
+/// Returns a list of Violation Ticket Statutes filtered by given section text (if provided).
+/// </summary>
+/// <param name="section">Motor vehicle act Section text to query by, ie "13(1)(a)" returns "Motor Vehicle or Trailer without Licence" contravention, or blank for no filter.</param>
+/// <param name="cancellationToken"></param>
+/// <returns></returns>
+[HttpGet]
     [Produces("application/json")]
     [ProducesResponseType(typeof(IList<Statute>), (int)HttpStatusCode.OK)]
     public async Task<IActionResult> StatutesAsync(string? section, CancellationToken cancellationToken)
