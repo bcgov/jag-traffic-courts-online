2aa8fc9e1c2b40f9c73a3ebbd83bef737a3505fc
Burak Karahan <burak.karahan@nttdata.com>
2022-06-07 11:27:04 -0400

    Added functionality to retrieve all disputes that can be filtered by specified jj assigned

--- src/backend/oracle-data-api/src/main/java/ca/bc/gov/open/jag/tco/oracledataapi/repository/DisputeRepository.java
+++ src/backend/oracle-data-api/src/main/java/ca/bc/gov/open/jag/tco/oracledataapi/repository/DisputeRepository.java
@@ -1,6 +1,7 @@
 package ca.bc.gov.open.jag.tco.oracledataapi.repository;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.repository.CrudRepository;
@@ -21,5 +22,8 @@ public interface DisputeRepository extends CrudRepository<Dispute, UUID> {
 
 	/** Fetch all records whose assignedTs has a timestamp older than the given date. */
     public Iterable<Dispute> findByAssignedTsBefore(Date olderThan);
+    
+    /** Fetch all records which have the specified jjAssigned. */
+    public List<Dispute> findByJjAssignedIgnoreCase(String jjAssigned);
 
 }
