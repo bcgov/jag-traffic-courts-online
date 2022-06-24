a48d3e7386e15452d7eb7c6ca97f27104309f54b
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2022-06-24 09:49:53 -0700

    jj-workbench & individual page

--- src/backend/TrafficCourts/Messaging/MessageContracts/DisputeSubmitted.cs
+++ src/backend/TrafficCourts/Messaging/MessageContracts/DisputeSubmitted.cs
@@ -8,6 +8,6 @@ namespace TrafficCourts.Messaging.MessageContracts
 {
     public class DisputeSubmitted : IMessage
     {
-        public long DisputeId { get; set; }
+        public long DisputeId { get; set; } = -1;
     }
 }
