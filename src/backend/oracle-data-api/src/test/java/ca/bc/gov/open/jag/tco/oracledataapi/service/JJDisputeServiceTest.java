4ed04e1c767fcd1e50b1f0e1aeae536d799685ce
Burak Karahan <burak.karahan@nttdata.com>
2022-07-21 11:07:31 -0400

    Added validation rules for updating statuses of JJ Disputes when an admin resolution is submitted. Added unit tests for JJ Dispute status update business rules

---
+++ src/backend/oracle-data-api/src/test/java/ca/bc/gov/open/jag/tco/oracledataapi/service/JJDisputeServiceTest.java
@@ -0,0 +1,91 @@
+package ca.bc.gov.open.jag.tco.oracledataapi.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ca.bc.gov.open.jag.tco.oracledataapi.BaseTestSuite;
+import ca.bc.gov.open.jag.tco.oracledataapi.error.NotAllowedException;
+import ca.bc.gov.open.jag.tco.oracledataapi.model.JJDispute;
+import ca.bc.gov.open.jag.tco.oracledataapi.model.JJDisputeStatus;
+import ca.bc.gov.open.jag.tco.oracledataapi.util.RandomUtil;
+
+class JJDisputeServiceTest extends BaseTestSuite {
+
+	@Autowired
+	private JJDisputeService jjDisputeService;
+
+	@ParameterizedTest
+	@EnumSource(value = JJDisputeStatus.class, names = { "NEW", "REVIEW", "IN_PROGRESS" })
+	void testSetStatusToIN_PROGRESS_200(JJDisputeStatus jjDisputeStatus) {
+		JJDispute jjDisputeToUpdate = saveDispute(jjDisputeStatus);
+		JJDispute jjDisputeWithUpdatedStatus = saveDispute(JJDisputeStatus.IN_PROGRESS);
+		jjDisputeService.updateJJDispute(jjDisputeToUpdate.getTicketNumber(), jjDisputeWithUpdatedStatus);
+	}
+
+	@ParameterizedTest
+	@EnumSource(value = JJDisputeStatus.class, names = { "COMPLETED" })
+	void testSetStatusToIN_PROGRESS_405(JJDisputeStatus jjDisputeStatus) {
+		JJDispute jjDisputeToUpdate = saveDispute(jjDisputeStatus);
+		JJDispute jjDisputeWithUpdatedStatus = saveDispute(JJDisputeStatus.IN_PROGRESS);
+		assertThrows(NotAllowedException.class, () -> {
+			jjDisputeService.updateJJDispute(jjDisputeToUpdate.getTicketNumber(), jjDisputeWithUpdatedStatus);
+		});
+	}
+	
+	@ParameterizedTest
+	@EnumSource(value = JJDisputeStatus.class, names = { "NEW", "REVIEW", "IN_PROGRESS" })
+	void testSetStatusToREVIEW_200(JJDisputeStatus jjDisputeStatus) {
+		JJDispute jjDisputeToUpdate = saveDispute(jjDisputeStatus);
+		JJDispute jjDisputeWithUpdatedStatus = saveDispute(JJDisputeStatus.REVIEW);
+		jjDisputeService.updateJJDispute(jjDisputeToUpdate.getTicketNumber(), jjDisputeWithUpdatedStatus);
+	}
+
+	@ParameterizedTest
+	@EnumSource(value = JJDisputeStatus.class, names = { "COMPLETED" })
+	void testSetStatusToREVIEW_405(JJDisputeStatus jjDisputeStatus) {
+		JJDispute jjDisputeToUpdate = saveDispute(jjDisputeStatus);
+		JJDispute jjDisputeWithUpdatedStatus = saveDispute(JJDisputeStatus.REVIEW);
+		assertThrows(NotAllowedException.class, () -> {
+			jjDisputeService.updateJJDispute(jjDisputeToUpdate.getTicketNumber(), jjDisputeWithUpdatedStatus);
+		});
+	}
+	
+	@ParameterizedTest
+	@EnumSource(value = JJDisputeStatus.class, names = { "REVIEW" })
+	void testSetStatusToCOMPLETED_200(JJDisputeStatus jjDisputeStatus) {
+		JJDispute jjDisputeToUpdate = saveDispute(jjDisputeStatus);
+		JJDispute jjDisputeWithUpdatedStatus = saveDispute(JJDisputeStatus.COMPLETED);
+		jjDisputeService.updateJJDispute(jjDisputeToUpdate.getTicketNumber(), jjDisputeWithUpdatedStatus);
+	}
+
+	@ParameterizedTest
+	@EnumSource(value = JJDisputeStatus.class, names = { "NEW", "COMPLETED", "IN_PROGRESS" })
+	void testSetStatusToCOMPLETED_405(JJDisputeStatus jjDisputeStatus) {
+		JJDispute jjDisputeToUpdate = saveDispute(jjDisputeStatus);
+		JJDispute jjDisputeWithUpdatedStatus = saveDispute(JJDisputeStatus.COMPLETED);
+		assertThrows(NotAllowedException.class, () -> {
+			jjDisputeService.updateJJDispute(jjDisputeToUpdate.getTicketNumber(), jjDisputeWithUpdatedStatus);
+		});
+	}
+
+	@Test
+	void testSetStatusToNEW_405() {
+		JJDispute jjDisputeToUpdate = saveDispute(JJDisputeStatus.NEW);
+		JJDispute jjDisputeWithUpdatedStatus = saveDispute(JJDisputeStatus.NEW);
+		assertThrows(NotAllowedException.class, () -> {
+			jjDisputeService.updateJJDispute(jjDisputeToUpdate.getTicketNumber(), jjDisputeWithUpdatedStatus);
+		});
+	}
+
+	private JJDispute saveDispute(JJDisputeStatus jjDisputeStatus) {
+		JJDispute jjDispute = RandomUtil.createJJDispute();
+		jjDispute.setStatus(jjDisputeStatus);
+
+		return jjDisputeRepository.save(jjDispute);
+	}
+
+}
