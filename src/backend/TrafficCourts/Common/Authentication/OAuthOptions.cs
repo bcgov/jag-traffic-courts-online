907bb78f11a791df1a6600744cfc68f4bd83958a
Phil Bolduc <pbolduc@users.noreply.github.com>
2022-08-09 14:40:57 -0700

    Add oauth for accessing keycloak

---
+++ src/backend/TrafficCourts/Common/Authentication/OAuthOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace TrafficCourts.Common.Authentication;
+
+public class OAuthOptions
+{
+    /// <summary>
+    /// The OAuth endpoint that issues the access tokens.
+    /// </summary>
+    public Uri AuthorizationUri { get; set; }
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+}
---
+++ src/backend/TrafficCourts/Common/Authentication/OAuthTokenCache.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System.Text;
+
+namespace TrafficCourts.Common.Authentication;
+
+public class OAuthTokenCache : TokenCache<OAuthOptions, Token>
+{
+    public OAuthTokenCache(IMemoryCache memoryCache, ILogger<OAuthTokenCache> logger) : base(memoryCache, logger)
+    {
+    }
+
+    protected override string GetCacheKey(OAuthOptions configuration)
+    {
+        StringBuilder buffer = new StringBuilder();
+
+        // create a cache key based on all the parameters
+        buffer.Append(configuration.AuthorizationUri);
+        buffer.Append('|');
+        buffer.Append(configuration.ClientId);
+        buffer.Append('|');
+        buffer.Append(configuration.ClientSecret);
+        buffer.Append('|');
+
+        return buffer.ToString();
+    }
+}
