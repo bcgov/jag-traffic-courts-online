e3eff1d4e70d463322282700bd3002b5cfdbf3b7
Phil Bolduc <pbolduc@users.noreply.github.com>
2022-08-15 15:35:29 -0700

    Exclude generated ArcDisputeClient from code coverage

---
+++ src/backend/TrafficCourts/TrafficCourts.Arc.Dispute.Client/ArcDisputeClient.CodeAnalysis.cs
@@ -0,0 +1,9 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TrafficCourts.Arc.Dispute.Client
+{
+    [ExcludeFromCodeCoverage(Justification = "Generated")]
+    public partial class ArcDisputeClient
+    {
+    }
+}
