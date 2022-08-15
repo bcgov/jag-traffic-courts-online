b59edd08738007f05250c273ffeac649abbdb6aa
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2022-08-15 10:24:10 -0700

    apis

---
+++ src/backend/TrafficCourts/Messaging/MessageContracts/DisputeCount.cs
@@ -0,0 +1,29 @@
+﻿using TrafficCourts.Common.OpenAPIs.OracleDataApi.v1_0;
+
+namespace TrafficCourts.Messaging.MessageContracts
+{
+    public class DisputeCount
+    {
+        /// <summary>
+        /// Represents the dispuant plea on count.
+        /// </summary>
+        public DisputeCountPleaCode PleaCode { get; set; }
+
+        /// <summary>
+        /// The disputant is requesting time to pay the ticketed amount.
+        /// </summary>
+        public DisputeCountRequestTimeToPay RequestTimeToPay { get; set; } = DisputeCountRequestTimeToPay.N;
+
+        /// <summary>
+        /// The disputant is requesting a reduction of the ticketed amount.
+        /// </summary>
+        public DisputeCountRequestReduction RequestReduction { get; set; } = DisputeCountRequestReduction.N;
+
+        /// <summary>
+        /// Does the want to appear in court?
+        /// </summary>
+        public DisputeCountRequestCourtAppearance RequestCourtAppearance { get; set; } = DisputeCountRequestCourtAppearance.N;
+
+        public string? DisputeType { get; set; }
+    }
+}
