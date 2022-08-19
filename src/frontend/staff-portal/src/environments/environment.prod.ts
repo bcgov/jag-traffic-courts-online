bc70833d841245bc5b4635b8d612b3c76326342a
Shark Wong <wtsunwai@gmail.com>
2022-08-19 10:16:38 -0700

    use environment to switch between dev and prod

--- src/frontend/staff-portal/src/environments/environment.prod.ts
+++ src/frontend/staff-portal/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 
 export const environment = {
-  production: true
+  production: true,
+  keycloakUrl: "https://dev.oidc.gov.bc.ca/auth", // change this for prod
+  keycloakRealm: "ezb8kej4", // change this for prod
+  keycloakClientId: "tco-staff-portal", // change this for prod
 };
