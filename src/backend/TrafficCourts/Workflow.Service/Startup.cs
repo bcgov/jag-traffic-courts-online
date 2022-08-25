038de381b3e63091a9c7d3df45729eab279251c0
Phil Bolduc <pbolduc@users.noreply.github.com>
2022-08-25 10:09:11 -0700

    Ensure oracle data api configuration is validated

--- src/backend/TrafficCourts/Workflow.Service/Startup.cs
+++ src/backend/TrafficCourts/Workflow.Service/Startup.cs
@@ -29,7 +29,7 @@ public static class Startup
 
         AddSwagger(builder, assembly, logger);
 
-        builder.Services.Configure<OracleDataApiConfiguration>(builder.Configuration.GetRequiredSection(OracleDataApiConfiguration.Section));
+        builder.Services.ConfigureValidatableSetting<OracleDataApiConfiguration>(builder.Configuration.GetRequiredSection(OracleDataApiConfiguration.Section));
         builder.Services.ConfigureValidatableSetting<EmailConfiguration>(builder.Configuration.GetSection(EmailConfiguration.Section));
         builder.Services.ConfigureValidatableSetting<SmtpConfiguration>(builder.Configuration.GetRequiredSection(SmtpConfiguration.Section));
 
