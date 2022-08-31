13371a19148fe1c2b70d44c333593c47f4c94cf5
Burak Karahan <burak.karahan@nttdata.com>
2022-08-31 14:39:23 -0400

    Added functionality to send authenticated user's full name from Staff API to Oracle Data API through a custom request header property and extract it to map user details to a new custom object which can be used as Principal to provide authenticated user details in Oracle Data API operations

--- src/backend/TrafficCourts/Staff.Service/Services/DisputeService.cs
+++ src/backend/TrafficCourts/Staff.Service/Services/DisputeService.cs
@@ -6,6 +6,7 @@ using TrafficCourts.Staff.Service.Configuration;
 using TrafficCourts.Staff.Service.Mappers;
 using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Security.Claims;
 
 namespace TrafficCourts.Staff.Service.Services;
 
@@ -46,6 +47,8 @@ public class DisputeService : IDisputeService
         var user = _httpContextAccessor.HttpContext?.User;
 
         var username = user?.Claims?.FirstOrDefault(_ => _.Type == "preferred_username")?.Value;
+        var fullName = user?.Claims?.FirstOrDefault(_ => _.Type == ClaimTypes.Name)?.Value;
+
         if (username is not null && !string.IsNullOrWhiteSpace(username))
         {
             // we expect the username to be of the form: someone@domain
@@ -57,6 +60,11 @@ public class DisputeService : IDisputeService
 
             HttpRequestHeaders requestHeaders = httpClient.DefaultRequestHeaders;
             requestHeaders.Add("x-username", username);
+
+            if (fullName is not null && !string.IsNullOrWhiteSpace(fullName))
+            {
+                requestHeaders.Add("fullName", fullName);
+            }
         }
         else
         {
--- src/backend/oracle-data-api/src/main/java/ca/bc/gov/open/jag/tco/oracledataapi/security/PreAuthenticatedToken.java
+++ src/backend/oracle-data-api/src/main/java/ca/bc/gov/open/jag/tco/oracledataapi/security/PreAuthenticatedToken.java
@@ -2,12 +2,14 @@ package ca.bc.gov.open.jag.tco.oracledataapi.security;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import ca.bc.gov.open.jag.tco.oracledataapi.model.CustomUserDetails;
+
 public class PreAuthenticatedToken extends UsernamePasswordAuthenticationToken {
 
 	private static final long serialVersionUID = 1L;
 
-	public PreAuthenticatedToken(String username) {
-		super(username, username);
+	public PreAuthenticatedToken(CustomUserDetails user) {
+		super(user, null);
 	}
 
 	@Override
