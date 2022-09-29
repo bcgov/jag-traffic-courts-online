88ca4684c2f74ba033331d1f398de792f3025f68
Burak Karahan <burak.karahan@nttdata.com>
2022-09-29 17:02:08 -0400

    Removed unnecessary tests

--- src/backend/TrafficCourts/TrafficCourts.Staff.Service.Test/Controllers/JJControllerTest.cs
+++ src/backend/TrafficCourts/TrafficCourts.Staff.Service.Test/Controllers/JJControllerTest.cs
@@ -21,32 +21,6 @@ namespace TrafficCourts.Staff.Service.Test.Controllers;
 public class JJControllerTest
 {
     [Fact]
-    public void AllEndpointsShouldImplementAuthorizeAttribute()
-    {
-        // Check all endpoints of JJDisputeController to confirm all are guarded with proper KeycloakAuthorization or explicit AllowAnonymous Attribute
-
-        // Arrange
-        var _endpoints = new List<(Type, MethodInfo)>(); // All endpoints to check in JJDisputeController
-
-        var assembly = Assembly.GetAssembly(typeof(JJController));
-        var allControllers = AllTypes.From(assembly).ThatDeriveFrom<VTCControllerBase<JJController>>();
-
-        foreach (Type t in allControllers)
-        {
-            var mInfos = t.GetMethods(BindingFlags.Public | BindingFlags.Instance).Where(x => x.DeclaringType!.Equals(t)).ToList();
-            foreach (MethodInfo mInfo in mInfos)
-                _endpoints.Add((t, mInfo));
-        }
-
-        // Act
-        var endpointsWithoutAuthorizeAttribute = _endpoints.Where(t => !t.Item2.IsDefined(typeof(KeycloakAuthorizeAttribute), false) && !t.Item2.IsDefined(typeof(AllowAnonymousAttribute), false)).ToList();
-        var brokenEndpoints = string.Join(" and ", endpointsWithoutAuthorizeAttribute.Select(x => x.Item2.Name));
-
-        // Assert
-        endpointsWithoutAuthorizeAttribute.Count.Should().Be(0, "because {0} should have the KeycloakAuthorization or Anonymous attribute", brokenEndpoints);
-    }
-
-    [Fact]
     public async void TestAssignJJDisputesToJJ200Result()
     {
         // Mock the OracleDataApi to update a specific JJDispute with ticket number (AJ201092461) to assign it to a JJ, confirm controller updates and assigns the JJ.
