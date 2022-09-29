8ed43e4dbacefc791f396bdb1c0f730adbe1bc1f
Phil Bolduc <pbolduc@users.noreply.github.com>
2022-09-29 08:50:23 -0700

    Remove unused using statements

--- src/backend/TrafficCourts/TrafficCourts.Common.Test/OpenAPIs/OracleDataApiConfigurationTest.cs
+++ src/backend/TrafficCourts/TrafficCourts.Common.Test/OpenAPIs/OracleDataApiConfigurationTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
-using TrafficCourts.Common.Configuration.Validation;
+﻿using TrafficCourts.Common.Configuration.Validation;
 using TrafficCourts.Common.OpenAPIs.OracleDataAPI;
 using Xunit;
 
