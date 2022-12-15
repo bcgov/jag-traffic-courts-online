bd5e8700189d020df5e914dd4e38122c43c4d1e5
Shark Wong <wtsunwai@gmail.com>
2022-12-15 15:53:04 -0800

    update angular unit test

--- src/frontend/citizen-portal/src/app/shared/components/page/page.component.spec.ts
+++ src/frontend/citizen-portal/src/app/shared/components/page/page.component.spec.ts
@@ -1,16 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { LoadingStore } from '@core/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { PageComponent } from './page.component';
 
 describe('PageComponent', () => {
   let component: PageComponent;
   let fixture: ComponentFixture<PageComponent>;
+  let store: MockStore;
+  let initialState = { ...LoadingStore.InitialState };
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PageComponent ]
+    TestBed.configureTestingModule({
+      declarations: [PageComponent],
+      providers: [provideMockStore({ initialState })],
     })
-    .compileComponents();
+
+    store = TestBed.inject(MockStore);
+    await TestBed.compileComponents();
   });
 
   beforeEach(() => {
