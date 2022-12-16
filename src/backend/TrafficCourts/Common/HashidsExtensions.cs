4ae51ffcbffd2c479468d54b32e8ee9581a6f927
Phil Bolduc <pbolduc@users.noreply.github.com>
2022-12-16 10:22:37 -0800

    Add xml doc comments for functions

--- src/backend/TrafficCourts/Common/HashidsExtensions.cs
+++ src/backend/TrafficCourts/Common/HashidsExtensions.cs
@@ -5,6 +5,31 @@ namespace TrafficCourts.Common
 {
     public static class HashidsExtensions
     {
+
+        /// <summary>
+        /// Encodes the provided Guid.
+        /// </summary>
+        /// <param name="hashids">The provider to encode</param>
+        /// <param name="value">The value to encode.</param>
+        /// <returns>The encoded Guid.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="hashids"/> is null</exception>
+        public static string EncodeGuid(this IHashids hashids, Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(hashids);
+
+            // "n" format specifier - 32 digits without hyphens, braces or parentheses
+            // example: 00000000000000000000000000000000
+            return hashids.EncodeHex(value.ToString("n"));
+        }
+
+        /// <summary>
+        /// Tries to decode a string into a value. 
+        /// </summary>
+        /// <param name="hashids"></param>
+        /// <param name="value">The string to decode.</param>
+        /// <param name="result"></param>
+        /// <returns><c>true</c> if <paramref name="value"/> was successfully decoded; otherwise, <c>false</c>.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="hashids"/> is null</exception>
         public static bool TryDecodeGuid(this IHashids hashids, [NotNullWhen(true)] string value, out Guid result)
         {
             ArgumentNullException.ThrowIfNull(hashids);
@@ -23,15 +48,6 @@ namespace TrafficCourts.Common
 
             result = default;
             return false;
-        }
-
-        public static string EncodeGuid(this IHashids hashids, Guid value)
-        {
-            ArgumentNullException.ThrowIfNull(hashids);
-
-            // "n" format specifier - 32 digits without hyphens, braces or parentheses
-            // example: 00000000000000000000000000000000
-            return hashids.EncodeHex(value.ToString("n"));
         }
     }
 }
