ade27feeb3489530986619217eedc7cd5cd4145d
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2022-12-19 12:35:45 -0800

    TCVP-2005 Added methods to retrieve PartId from idirUsername

--- src/backend/TrafficCourts/Staff.Service/Services/IKeycloakService.cs
+++ src/backend/TrafficCourts/Staff.Service/Services/IKeycloakService.cs
@@ -12,4 +12,16 @@ public interface IKeycloakService
     /// <exception cref="ApiException">A server side error occurred.</exception>
     Task<ICollection<UserRepresentation>> UsersByGroupAsync(string groupName, CancellationToken cancellationToken);
 
+    /// <summary>Returns all users whose idir_username matches the provided idir.</summary>
+    /// <param name="idirUsername"></param>
+    /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+    /// <returns>The collection of UserRepresentation records.</returns>
+    /// <exception cref="ApiException">A server side error occurred.</exception>
+    Task<ICollection<UserRepresentation>> UsersByIdirAsync(string idirUsername, CancellationToken cancellationToken);
+
+    /// <summary>Extracts the attributes named "part_id" from the given UserRepresentation object.</summary>
+    /// <param name="userRepresentation"></param>
+    /// <returns>The part_id attribute of the given user.</returns>
+    ICollection<string> TryGetPartIds(UserRepresentation userRepresentation);
+
 }
