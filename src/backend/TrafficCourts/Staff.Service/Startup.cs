9b7e923459e94c21add0a3626f245af372a6f455
Burak Karahan <burak.karahan@nttdata.com>
2023-01-11 15:14:43 -0500

    Added dependency injection for IComsService in staff api

--- src/backend/TrafficCourts/Staff.Service/Startup.cs
+++ src/backend/TrafficCourts/Staff.Service/Startup.cs
@@ -62,6 +62,7 @@ public static class Startup
         builder.Services.AddTransient<IFileHistoryService, FileHistoryService>();
         builder.Services.AddTransient<IEmailHistoryService, EmailHistoryService>();
         builder.Services.AddTransient<IJJDisputeService, JJDisputeService>();
+        builder.Services.AddTransient<IComsService, ComsService>();
 
         // staff service should not be creating emails, should send messages for workflow
         builder.Services.AddEmailTemplates();
