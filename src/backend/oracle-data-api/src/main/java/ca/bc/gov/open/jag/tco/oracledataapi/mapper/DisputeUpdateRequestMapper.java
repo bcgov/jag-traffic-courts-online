d6716d628309e4fc82177671b1150128945be3ff
Burak Karahan <burak.karahan@nttdata.com>
2023-02-06 14:42:19 -0500

    Added mapper for converting from ORDS disputeUpdateRequest model to Oracle Data API disputeUpdateRequest model and vice versa. Added JUnit tests for the mapper

--- src/backend/oracle-data-api/src/main/java/ca/bc/gov/open/jag/tco/oracledataapi/mapper/DisputeUpdateRequestMapper.java
+++ src/backend/oracle-data-api/src/main/java/ca/bc/gov/open/jag/tco/oracledataapi/mapper/DisputeUpdateRequestMapper.java
@@ -3,12 +3,15 @@ package ca.bc.gov.open.jag.tco.oracledataapi.mapper;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import ca.bc.gov.open.jag.tco.oracledataapi.model.DisputeUpdateRequest;
+import ca.bc.gov.open.jag.tco.oracledataapi.model.DisputeUpdateRequestStatus;
+import ca.bc.gov.open.jag.tco.oracledataapi.model.DisputeUpdateRequestType;
 
 /**
- * This mapper maps from ORDS disputeUpdateRequest model to Oracle Data API disputeUpdateRequest model
+ * This mapper maps from ORDS disputeUpdateRequest model to Oracle Data API disputeUpdateRequest model and vice versa
  */
 @Mapper
 (componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR) // This is required for tests to work
@@ -19,12 +22,56 @@ public interface DisputeUpdateRequestMapper {
 	// Map disputeUpdateRequest data from ORDS to Oracle Data API disputeUpdateRequest model
 	@Mapping(source = "disputeUpdateRequestId", target = "disputeUpdateRequestId")
 	@Mapping(source = "disputeId", target = "disputeId")
-	@Mapping(source = "disputeUpdateStatTypeCd", target = "status")
-	@Mapping(source = "disputeUpdateReqTypeCd", target = "updateType")
+	@Mapping(source = "disputeUpdateStatTypeCd", target = "status", qualifiedByName="mapDisputeUpdateStatType")
+	@Mapping(source = "disputeUpdateReqTypeCd", target = "updateType", qualifiedByName="mapDisputeUpdateReqType")
 	@Mapping(source = "requestJsonTxt", target = "updateJson")
 	@Mapping(source = "entUserId", target = "createdBy")
 	@Mapping(source = "entDtm", target = "createdTs")
 	@Mapping(source = "updUserId", target = "modifiedBy")
 	@Mapping(source = "updDtm", target = "modifiedTs")
 	DisputeUpdateRequest convert(ca.bc.gov.open.jag.tco.oracledataapi.ords.occam.api.model.DisputeUpdateRequest disputeUpdateRequest);
+
+	// Map Oracle Data API disputeUpdateRequest model to ORDS isputeUpdateRequest data
+	@Mapping(target = "disputeUpdateRequestId", source = "disputeUpdateRequestId")
+	@Mapping(target = "disputeId", source = "disputeId")
+	@Mapping(target = "disputeUpdateStatTypeCd", source = "status", qualifiedByName="mapDisputeUpdateStatTypeCd")
+	@Mapping(target = "disputeUpdateReqTypeCd", source = "updateType", qualifiedByName="mapDisputeUpdateReqTypeCd")
+	@Mapping(target = "requestJsonTxt", source = "updateJson")
+	@Mapping(target = "entUserId", source = "createdBy")
+	@Mapping(target = "entDtm", source = "createdTs")
+	@Mapping(target = "updUserId", source = "modifiedBy")
+	@Mapping(target = "updDtm", source = "modifiedTs")
+	ca.bc.gov.open.jag.tco.oracledataapi.ords.occam.api.model.DisputeUpdateRequest convert(DisputeUpdateRequest disputeUpdateRequest);
+
+	@Named("mapDisputeUpdateStatType")
+	default DisputeUpdateRequestStatus mapDisputeUpdateStatType(String statusShortCd) {
+		DisputeUpdateRequestStatus[] values = DisputeUpdateRequestStatus.values();
+		for (DisputeUpdateRequestStatus status : values) {
+			if (status.getShortName().equals(statusShortCd)) {
+				return status;
+			}
+		}
+		return null;
+	}
+
+	@Named("mapDisputeUpdateReqType")
+	default DisputeUpdateRequestType mapDisputeUpdateReqType(String statusShortCd) {
+		DisputeUpdateRequestType[] values = DisputeUpdateRequestType.values();
+		for (DisputeUpdateRequestType type : values) {
+			if (type.getShortName().equals(statusShortCd)) {
+				return type;
+			}
+		}
+		return null;
+	}
+
+	@Named("mapDisputeUpdateStatTypeCd")
+	default String mapDisputeUpdateStatTypeCd(DisputeUpdateRequestStatus status) {
+		return status.getShortName();
+	}
+
+	@Named("mapDisputeUpdateReqTypeCd")
+	default String mapDisputeUpdateReqTypeCd(DisputeUpdateRequestType status) {
+		return status.getShortName();
+	}
 }
