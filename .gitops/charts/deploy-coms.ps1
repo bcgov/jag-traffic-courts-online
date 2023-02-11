ee99d5628414bca72d97a23bbf1e8ed32ed1277f
Phil Bolduc <pbolduc@users.noreply.github.com>
2023-02-10 16:42:12 -0800

    Update coms deployment for dev, test. Update docs

--- .gitops/charts/deploy-coms.ps1
+++ .gitops/charts/deploy-coms.ps1
@@ -9,7 +9,7 @@ oc project $plate-$environment 2>&1 | Out-Null
 
 if ($LastExitCode -eq 0) {
   # install or upgrade the application
-  helm upgrade coms ./common-object-management-service-0.0.6.tgz --install --values common-object-management-service-values.yaml
+  helm upgrade coms ./common-object-management-service-0.0.8.tgz --install --values coms-$environment-values.yaml
 } else {
     Write-Error "Could not change to project $plate-$environment"
 }
