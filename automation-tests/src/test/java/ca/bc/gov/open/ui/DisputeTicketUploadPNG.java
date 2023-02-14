1067bd70ba70177c5eb2e100cff4c11687483613
claudiuvlasceanu <43979009+claudiuvlasceanu@users.noreply.github.com>
2023-02-14 15:09:43 -0800

    upload file test

---
+++ automation-tests/src/test/java/ca/bc/gov/open/ui/DisputeTicketUploadPNG.java
@@ -0,0 +1,135 @@
+package ca.bc.gov.open.ui;
+
+import java.time.Duration;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import ca.bc.gov.open.cto.CommonUtils;
+import ca.bc.gov.open.cto.WebDriverManager;
+
+public class DisputeTicketUploadPNG {
+
+	private WebDriver driver;
+
+	@After
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		WebDriverManager.instance = null;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void test() throws Exception {
+		driver = WebDriverManager.getDriver();
+		WebDriverWait driverWait = WebDriverManager.getDriverWait();
+		WebElement element = WebDriverManager.getElement();
+		WebDriverManager.getElements();
+
+		CommonUtils.login();
+
+		Thread.sleep(1000);
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		jse.executeScript("scroll(0, 450);");
+		Thread.sleep(1000);
+		new WebDriverWait(driver, Duration.ofSeconds(10)).until(
+				ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), ' Dispute your ticket ')]")))
+				.click();
+
+		new WebDriverWait(driver, Duration.ofSeconds(10)).until(
+				ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), ' Traffic Ticket ')]")))
+				.click();
+
+		new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//*[contains(text(), ' Upload ticket image ')]")));
+
+		Thread.sleep(1000);
+		WebElement upload = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("file")));
+		upload.sendKeys(System.getProperty("user.dir") + '/' + "testTicket.png");
+
+		new WebDriverWait(driver, Duration.ofSeconds(20)).until(
+				ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Ticket details')]")));
+		new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'AR74712274')]")));
+		new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Feb 02, 2023')]")));
+		new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Brown')]")));
+		new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'SK')]")));
+		new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), '19:44')]")));
+		new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Jackson')]")));
+		new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), '9949830')]")));
+		new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions
+				.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Fail To Notify Vehicle Change')]")));
+		System.out.println("File uploaded properly");
+
+		Thread.sleep(1000);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(
+				By.xpath("//*[contains(text(), ' Save information and create online ticket ')]")));
+		js.executeScript("arguments[0].click();", element);
+
+		Thread.sleep(1000);
+		// Start Traffic ticket dispute request
+		JavascriptExecutor js1 = (JavascriptExecutor) driver;
+		// Scroll down till the bottom of the page
+		js1.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+		System.out.println("Scroll down till the bottom of the page");
+		Thread.sleep(1000);
+		element = driverWait
+				.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".mat-button-wrapper > strong")));
+		element.click();
+		System.out.println("Start dispute ticket");
+
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("mat-input-4")));
+		element.sendKeys("3220 Qadra");
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("mat-input-5")));
+		element.sendKeys("Victoria");
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("mat-input-7")));
+		element.sendKeys("V8X1G3");
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("mat-input-6")));
+		element.sendKeys("claudiu.vlasceanu@nttdata.com");
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("mat-input-8")));
+		element.sendKeys("9999999999");
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("mat-input-9")));
+		element.sendKeys("999 999 1234");
+
+		DisputeTicketOptionsPicker review = new DisputeTicketOptionsPicker();
+		review.reviewProcess(element, driverWait, driver);
+		
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("mat-input-10")));
+		element.sendKeys(
+				"semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel,");
+		// Click Next
+		JavascriptExecutor js5 = (JavascriptExecutor) driver;
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("primaryButton")));
+		js5.executeScript("arguments[0].click();", element);
+		
+		System.out.println("Click Submit button");
+		Thread.sleep(1000);
+		JavascriptExecutor js3 = (JavascriptExecutor) driver;
+		element = driverWait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Submit request')]")));
+		js3.executeScript("arguments[0].click();", element);
+
+		DisputeTicketOptionsPicker popup = new DisputeTicketOptionsPicker();
+		popup.popupSubmitWindow(element, driverWait, driver);
+
+	}
+
+}
