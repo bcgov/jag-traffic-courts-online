48712bbf30d7dfac5da2b7389c884a7082a24858
Phil Bolduc <philbolduc@gmail.com>
2023-03-24 08:30:53 -0700

    Add validation for RequestTimeToPay and RequestCourtAppearance, add unit tests, tidy rules

---
+++ src/backend/TrafficCourts/Test/Citizen.Service/Validators/NoticeOfDisputeValidatorTest.cs
@@ -0,0 +1,35 @@
+﻿using TrafficCourts.Citizen.Service.Models.Disputes;
+using TrafficCourts.Citizen.Service.Validators;
+using Xunit;
+using FluentValidation.TestHelper;
+using FluentValidation;
+using System.Collections.Generic;
+
+namespace TrafficCourts.Test.Citizen.Service.Validators;
+
+public class NoticeOfDisputeValidatorTest : ValidatorTest<NoticeOfDispute, NoticeOfDisputeValidator>
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public void TicketNumber_must_be_not_null_or_empty(string? value)
+    {
+        NoticeOfDispute model = new() { TicketNumber = value };
+
+        var result = _sut.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor(_ => _.TicketNumber)
+            .WithErrorCode(NotEmptyValidator);
+    }
+
+    [Fact]
+    public void TicketNumber_must_have_max_length_of_12()
+    {
+        NoticeOfDispute model = new() { TicketNumber = new string('A', 13) };
+
+        var result = _sut.TestValidate(model);
+
+        result.ShouldHaveValidationErrorFor(_ => _.TicketNumber)
+            .WithErrorCode(MaximumLengthValidator);
+    }
+}
---
+++ src/backend/TrafficCourts/Test/Citizen.Service/Validators/ValidatorTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace TrafficCourts.Test.Citizen.Service.Validators;
+
+public abstract class ValidatorTest<TModel, TValidator> where TValidator : AbstractValidator<TModel>, new()
+{
+    protected readonly TValidator _sut = new TValidator();
+
+    protected const string EnumValidator = "EnumValidator";
+    protected const string MaximumLengthValidator = "MaximumLengthValidator";
+    protected const string NotEmptyValidator = "NotEmptyValidator";
+    protected const string NotNullValidator = "NotNullValidator";
+    /// <summary>
+    /// User supplied predicate in a Must() call.
+    /// </summary>
+    protected const string PredicateValidator = "PredicateValidator";
+}
