6c473c4ad673b7c356f328ecab43486edf034512
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2023-03-31 16:48:31 -0700

    updates

---
+++ src/backend/TrafficCourts/Common/Features/Lookups/AgencyLookup.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using TrafficCourts.Common.Models;
+
+namespace TrafficCourts.Common.Features.Lookups;
+
+public static class AgencyLookup
+{
+    public class Request : IRequest<Response>
+    {
+        public Request()
+        {
+        }
+    }
+
+    public class Response
+    {
+        public Response(IList<Agency> agencies)
+        {
+            Agencies = agencies;
+        }
+
+        public IList<Agency> Agencies { get; }
+    }
+
+    public class Handler : IRequestHandler<Request, Response>
+    {
+        private readonly IAgencyLookupService _service;
+
+        public Handler(IAgencyLookupService service)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        {
+            IList<Agency> agencies = await _service.GetListAsync();
+            return new Response(agencies);
+        }
+    }
+}
---
+++ src/backend/TrafficCourts/Common/Features/Lookups/IAgencyLookupService.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using TrafficCourts.Common.Models;
+
+namespace TrafficCourts.Common.Features.Lookups;
+
+public interface IAgencyLookupService : ICachedLookupService<Agency>
+{
+}
