a14ec7e83add867533555fe490a33e4bf583faad
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2023-04-17 08:16:05 -0700

    bus

---
+++ src/backend/TrafficCourts/Common/Features/Lookups/ProvinceLookup.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using TrafficCourts.Common.Models;
+
+namespace TrafficCourts.Common.Features.Lookups;
+
+public static class ProvinceLookup
+{
+    public class Request : IRequest<Response>
+    {
+        public Request()
+        {
+        }
+    }
+
+    public class Response
+    {
+        public Response(IList<Province> agencies)
+        {
+            Provinces = agencies;
+        }
+
+        public IList<Province> Provinces { get; }
+    }
+
+    public class Handler : IRequestHandler<Request, Response>
+    {
+        private readonly IProvinceLookupService _service;
+
+        public Handler(IProvinceLookupService service)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+        }
+
+        public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+        {
+            IList<Province> agencies = await _service.GetListAsync();
+            return new Response(agencies);
+        }
+    }
+}
---
+++ src/backend/TrafficCourts/Common/Features/Lookups/ProvinceLookupService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using TrafficCourts.Common.Models;
+
+namespace TrafficCourts.Common.Features.Lookups;
+
+public class ProvinceLookupService : CachedLookupService<Province>, IProvinceLookupService
+{
+    public ProvinceLookupService(IConnectionMultiplexer redis, IMemoryCache cache, ILogger<ProvinceLookupService> logger)
+        : base("Provinces", redis, cache, TimeSpan.FromHours(1), logger)
+    {
+    }
+}
