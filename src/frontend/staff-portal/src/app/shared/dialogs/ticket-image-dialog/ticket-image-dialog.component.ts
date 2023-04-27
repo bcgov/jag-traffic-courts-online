59b877c153dd0aa538b1b22d0a1c4113ad2d627b
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2023-04-27 16:05:43 -0700

    simplify

--- src/frontend/staff-portal/src/app/shared/dialogs/ticket-image-dialog/ticket-image-dialog.component.ts
+++ src/frontend/staff-portal/src/app/shared/dialogs/ticket-image-dialog/ticket-image-dialog.component.ts
@@ -1,28 +1,19 @@
 import {
-  AfterViewInit,
   Component, Inject
 } from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {MAT_DIALOG_DATA} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-ticket-image-dialog',
   templateUrl: './ticket-image-dialog.component.html',
   styleUrls: ['./ticket-image-dialog.component.scss'],
 })
-export class TicketImageDialogComponent implements AfterViewInit {
+export class TicketImageDialogComponent {
   // to hide bug: mat-expansion-panel animates expanded to closed when nested in a mat-dialog
-  public visiblePanel = false;
   public imageToShow: any;
 
   constructor(
-    private dialogRef: MatDialogRef<TicketImageDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data) {
-      this.imageToShow = data.imageToShow;
-    //
-  }
-
-  public ngAfterViewInit(): void {
-    // timeout required to avoid the dreaded 'ExpressionChangedAfterItHasBeenCheckedError'
-    setTimeout(() => (this.visiblePanel = true));
+      this.imageToShow = data.imageToShow; // pass in ticket image
   }
 }
