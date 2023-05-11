7fde87ec7b4e67d52eb208f53f9f05152a94ce9d
Lorraine Dame <102263532+lfdgov@users.noreply.github.com>
2023-05-11 12:04:43 -0700

    update pizza tracker

--- src/frontend/citizen-portal/src/app/shared/dialogs/dispute-status-dialog/dispute-status-dialog.component.ts
+++ src/frontend/citizen-portal/src/app/shared/dialogs/dispute-status-dialog/dispute-status-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { QueryParamsForSearch } from '@shared/models/query-params-for-search.model';
-import { DisputeStatus, JJDisputeStatus, SearchDisputeResult } from 'app/api';
+import { DisputeStatus, JJDisputeHearingType, JJDisputeStatus, SearchDisputeResult } from 'app/api';
 import { StatusStepType } from 'app/services/dispute.service';
 import { DisputeStore } from 'app/store';
 
@@ -15,64 +15,94 @@ export class DisputeStatusDialogComponent {
   params: QueryParamsForSearch;
   steps: StatusStep[] = [];
   selectedStep = 0;
+  JJDisputeStatus = JJDisputeStatus;
+  DisputeStatus = DisputeStatus;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) private dialogData: DisputeStore.State,
   ) {
     this.dispute = this.dialogData.result;
     this.params = this.dialogData.params;
-    Object.values(StatusStepType).forEach((type, index) => {
-      let step: StatusStep = { seq: index, type, isCompleted: false };
-      this.steps.push(step);
-    })
     this.markStepCompleted();
   }
 
   private markStepCompleted(): void {
-    let steps = [...this.steps].reverse();
-    let lastCompletedStepIndex = 0;
-    steps.forEach((step, i) => {
-      let isCompleted = false;
-      let index = steps.length - 1 - i;
+    Object.values(StatusStepType).forEach((type, index) => {
+      let step: StatusStep = { seq: index, type, isCompleted: false };
       switch (step.type) {
         case StatusStepType.SUBMITTED: { // must be submitted
-          isCompleted = true;
+          step.isCompleted = true;
+          this.steps.push(step);
+          break;
         }
         case StatusStepType.PROCESSING: {
-          let statuses: DisputeStatus[] = [DisputeStatus.Processing];
-          if (statuses.indexOf(this.dispute?.dispute_status) > -1) {
-            isCompleted = true;
-          }
+          step.isCompleted = this.checkProcessingCompleted();
+          if ([DisputeStatus.Cancelled].indexOf(this.dispute?.dispute_status) <= -1) this.steps.push(step);
           break;
         }
         case StatusStepType.SCHEDULED: {
-          let jjStatuses: JJDisputeStatus[] = [
-            JJDisputeStatus.HearingScheduled,
-            JJDisputeStatus.InProgress,
-            JJDisputeStatus.RequireCourtHearing,
-            JJDisputeStatus.RequireMoreInfo,
-            JJDisputeStatus.Review,
-          ];
-
-          if (jjStatuses.indexOf(this.dispute?.jjdispute_status) > -1) {
-            isCompleted = true;
-          }
+          step.isCompleted = this.checkScheduledCompleted();
+          if (!this.checkCancelled() && !this.checkConcluded() && this.dispute?.hearing_type === JJDisputeHearingType.CourtAppearance) this.steps.push(step);
           break;
         }
         case StatusStepType.CONFIRMED: {
-          let jjStatuses: JJDisputeStatus[] = [JJDisputeStatus.Confirmed];
-          if (jjStatuses.indexOf(this.dispute?.jjdispute_status) > -1) {
-            isCompleted = true;
+          step.isCompleted = this.checkConfirmedCompleted();
+          if (!this.checkCancelled() && !this.checkConcluded()) this.steps.push(step);
+          break;
+        }
+        case StatusStepType.CANCELLED: {
+          step.isCompleted = this.checkCancelled();
+          if (step.isCompleted) this.steps.push(step);
+          break;
           }
+        case StatusStepType.CONCLUDED: {
+          step.isCompleted = this.checkConcluded();
+          if (step.isCompleted) this.steps.push(step);
           break;
         }
       }
-      if (isCompleted || index < lastCompletedStepIndex) {
-        step.isCompleted = true;
-        lastCompletedStepIndex = lastCompletedStepIndex === 0 ? index : lastCompletedStepIndex;
-      }
-    })
-    this.selectedStep = lastCompletedStepIndex;
+      if (step.isCompleted) this.selectedStep = this.steps.length -1;
+    });
+  }
+
+  private checkProcessingCompleted(): boolean {
+    if (this.checkScheduledCompleted()) return true;
+    else {
+      let statuses: DisputeStatus[] = [DisputeStatus.Processing];
+      if (statuses.indexOf(this.dispute?.dispute_status) > -1) {
+        return true;
+      } else return false;
+    }
+  }
+
+  private checkScheduledCompleted(): boolean {
+    if (this.checkConfirmedCompleted()) return true;
+    else {
+      let jjStatuses: JJDisputeStatus[] = [
+        JJDisputeStatus.HearingScheduled,
+        JJDisputeStatus.InProgress,
+        JJDisputeStatus.RequireCourtHearing,
+        JJDisputeStatus.RequireMoreInfo,
+        JJDisputeStatus.Review,
+      ];
+      if (jjStatuses.indexOf(this.dispute?.jjdispute_status) > -1) {
+        return true;
+      } else return false;
+    }
+  }
+
+  private checkConfirmedCompleted(): boolean {
+    if (this.dispute?.jjdispute_status === JJDisputeStatus.Confirmed) return true;
+    else return false;
+  }
+
+  private checkCancelled(): boolean {
+    if ([DisputeStatus.Cancelled].indexOf(this.dispute?.dispute_status) > -1 || [JJDisputeStatus.Cancelled].indexOf(this.dispute?.jjdispute_status) > -1) return true;
+    else return false;
+  }
+  private checkConcluded(): boolean {
+    if ([JJDisputeStatus.Concluded].indexOf(this.dispute?.jjdispute_status) > -1) return true;
+    else return false;
   }
 }
 
