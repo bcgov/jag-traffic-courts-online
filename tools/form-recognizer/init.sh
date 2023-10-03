2e8bab384e3ecb415e72d3453bd44824af8af45b
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2023-10-03 12:35:35 -0700

    Moved v4 model to 2022-04 in preparation for a 2023-09 VT model

--- tools/form-recognizer/init.sh
+++ tools/form-recognizer/init.sh
@@ -2,5 +2,6 @@
 # Seed the shared folder that contains the ocr model on startup
 if [ ! -f /shared/init.lock ]; then
   touch /shared/init.lock
+  rm -rf /shared/{*,.*}
   cp -r /shared-src/* /shared
 fi
