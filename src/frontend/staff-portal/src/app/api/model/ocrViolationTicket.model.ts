da569c4ecd4cbf3534b05ae3cd657f21d0000d07
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2023-10-23 14:03:17 -0700

    Regenerated the staff-api open-api spec in staff-portal

--- src/frontend/staff-portal/src/app/api/model/ocrViolationTicket.model.ts
+++ src/frontend/staff-portal/src/app/api/model/ocrViolationTicket.model.ts
@@ -10,9 +10,11 @@
  * Do not edit the class manually.
  */
 import { Field } from './field.model';
+import { ViolationTicketVersion } from './violationTicketVersion.model';
 
 
 export interface OcrViolationTicket { 
+    ticketVersion?: ViolationTicketVersion;
     imageFilename?: string | null;
     globalConfidence?: number;
     fields?: { [key: string]: Field; } | null;
