4e54d41b93a946fa22704036a58fe6e4a9903b70
Phil Bolduc <pbolduc@users.noreply.github.com>
2023-10-30 12:44:20 -0700

    Disable integration tests by default

---
+++ src/backend/TrafficCourts/TrafficCourts.Staff.Service.Test/Services/IntegrationTestFactAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using Xunit;
+
+namespace TrafficCourts.Staff.Service.Test.Services;
+
+public sealed class IntegrationTestFactAttribute : FactAttribute
+{
+    private static readonly Mode _mode;
+
+    static IntegrationTestFactAttribute()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<IntegrationTestFactAttribute>()
+            .Build();
+
+        _mode = configuration.GetSection("IntegrationTest:Mode").Get<Mode>();
+    }
+    public IntegrationTestFactAttribute()
+    {
+        if (_mode == Mode.Skip)
+        {
+            Skip = "Integration test not configured, set IntegrationTest:Mode=Container to user secret to enable";
+        }
+    }
+
+    public enum Mode
+    {
+        Skip = 0,
+        Container
+    }
+}
