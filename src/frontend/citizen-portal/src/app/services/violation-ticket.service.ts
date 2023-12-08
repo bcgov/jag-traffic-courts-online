26dd7fabb91240f1a1ae942f65a9b341e824447a
Kevin Neufeld <Kevin.Neufeld@nttdata.com>
2023-12-08 10:50:02 -0800

    TCVP-2629 UI javascript error

--- src/frontend/citizen-portal/src/app/services/violation-ticket.service.ts
+++ src/frontend/citizen-portal/src/app/services/violation-ticket.service.ts
@@ -400,9 +400,9 @@ export class ViolationTicketService {
   }
 
   private isErrorMatch(err: HttpErrorResponse, msg: string, exactMatch: boolean = true) {
-    return exactMatch ?
-      (err.error.errors?.includes(msg) || err.error.includes(msg)) :
-      (err.error.errors?.filter(i => i.indexOf(msg) > -1).length > 0) || err.error.indexOf(msg) >= 0;
+    return exactMatch
+      ? (err.error.errors?.includes(msg) || (typeof err.error.includes === "function" && err.error.includes(msg)))
+      : (err.error.errors?.filter(i => i.indexOf(msg) > -1).length > 0) || (typeof err.error.indexOf === "function" && err.error.indexOf(msg) >= 0);
   }
 
   private openImageTicketNotFoundDialog(title: string, key: string) {
