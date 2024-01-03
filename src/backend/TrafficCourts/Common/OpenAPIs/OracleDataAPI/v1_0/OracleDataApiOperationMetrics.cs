d1d48023a5ea3ee818f98a1d971b822df5beba6d
Phil Bolduc <philbolduc@gmail.com>
2024-01-03 14:31:22 -0800

    Trying to fix oracle data api metrics genertion

--- src/backend/TrafficCourts/Common/OpenAPIs/OracleDataAPI/v1_0/OracleDataApiOperationMetrics.cs
+++ src/backend/TrafficCourts/Common/OpenAPIs/OracleDataAPI/v1_0/OracleDataApiOperationMetrics.cs
@@ -5,9 +5,9 @@ namespace TrafficCourts.Common.OpenAPIs.OracleDataAPI.v1_0
 {
     public class OracleDataApiOperationMetrics : OperationMetrics, IOracleDataApiOperationMetrics
     {
-        public const string MeterName = "oracledataapi";
+        public const string MeterName = "OracleDataApi";
 
-        public OracleDataApiOperationMetrics(IMeterFactory factory) : base(factory, MeterName, "Oracle Data Api")
+        public OracleDataApiOperationMetrics(IMeterFactory factory) : base(factory, MeterName, "oracledataapi", "Oracle Data Api")
         {
         }
     }
