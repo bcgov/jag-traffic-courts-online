d3ae1db6683ae18cd8c8392391edf424bc7b7344
Sai Krishna Metpalli <310911@NTTDATA.COM>
2024-01-10 08:01:03 -0800

    TCVP-2671: added translate module

--- src/frontend/citizen-portal/src/app/shared/components/resolution-footer/resolution-footer.component.spec.ts
+++ src/frontend/citizen-portal/src/app/shared/components/resolution-footer/resolution-footer.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResolutionFooterComponent } from './resolution-footer.component';
+import { NgxMaterialLegacyModule as NgxMaterialModule } from '@shared/modules/ngx-material/ngx-material.legacy.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 describe('ResolutionFooterComponent', () => {
   let component: ResolutionFooterComponent;
@@ -8,6 +10,7 @@ describe('ResolutionFooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [NgxMaterialModule, TranslateModule.forRoot()],
       declarations: [ResolutionFooterComponent],
     }).compileComponents();
   });
