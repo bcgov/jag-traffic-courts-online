8341e2ad35e43a0430317cd42501364fb6dd04e8
Phil Bolduc <philbolduc@gmail.com>
2024-01-22 12:40:04 -0800

    Prevent multiple updates after email verified already

---
+++ src/backend/TrafficCourts/Workflow.Service/Consumers/TagProvider.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using TrafficCourts.Messaging.MessageContracts;
+
+namespace TrafficCourts.Workflow.Service;
+
+/// <summary>
+/// Logging tag provider for adding properties to log messages.
+/// </summary>
+internal static partial class TagProvider
+{
+    public static void RecordTags(ITagCollector collector, ConsumeContext<EmailVerificationSuccessful> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+    }
+
+    public static void RecordTags(ITagCollector collector, ConsumeContext<CheckEmailVerificationTokenRequest> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+    }
+}
---
+++ src/backend/TrafficCourts/Workflow.Service/Sagas/TagProvider.cs
@@ -0,0 +1,48 @@
+﻿using MassTransit;
+using TrafficCourts.Messaging.MessageContracts;
+using TrafficCourts.Workflow.Service.Sagas;
+
+namespace TrafficCourts.Workflow.Service;
+
+/// <summary>
+/// Logging tag provider for adding properties to log messages.
+/// </summary>
+internal static partial class TagProvider
+{
+    public static void RecordTags(ITagCollector collector, BehaviorContext<VerifyEmailAddressState, SubmitNoticeOfDispute> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+    }
+
+    public static void RecordTags(ITagCollector collector, BehaviorContext<VerifyEmailAddressState, RequestEmailVerification> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+    }
+
+    public static void RecordTags(ITagCollector collector, BehaviorContext<VerifyEmailAddressState, CheckEmailVerificationTokenRequest> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+    }
+
+    public static void RecordTags(ITagCollector collector, BehaviorContext<VerifyEmailAddressState, EmailVerificationSuccessful> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+    }
+
+    public static void RecordTags(ITagCollector collector, BehaviorContext<VerifyEmailAddressState, ResendEmailVerificationEmail> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+    }
+
+    public static void RecordTags(ITagCollector collector, BehaviorContext<VerifyEmailAddressState, NoticeOfDisputeSubmitted> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+    }
+
+    public static void RecordTags(ITagCollector collector, BehaviorContext<VerifyEmailAddressState, SendEmailVerificationFailed> context)
+    {
+        collector.Add(nameof(context.Message.NoticeOfDisputeGuid), context.Message.NoticeOfDisputeGuid);
+        collector.Add(nameof(context.Message.Reason), context.Message.Reason);
+    }
+
+}
