ecb0b00a1edecacd8fee5750955123b37a5ba679
Phil Bolduc <philbolduc@gmail.com>
2024-01-25 10:15:44 -0800

    TCVP-2642 enhance logging of access token handling

---
+++ src/backend/TrafficCourts/TrafficCourts.Core/Http/TagProvider.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Net;
+using TrafficCourts.Core.Http.Models;
+
+namespace TrafficCourts.Http;
+
+/// <summary>
+/// Provides logging tags for 
+/// </summary>
+internal static partial class TagProvider
+{
+    public static void RecordTags(ITagCollector collector, Token token)
+    {
+        if (token is not null)
+        {
+            collector.Add(nameof(token.AccessToken), token.AccessToken);
+            collector.Add(nameof(token.RefreshExpiresIn), token.RefreshExpiresIn);
+            collector.Add(nameof(token.RefreshToken), token.RefreshToken);
+            collector.Add(nameof(token.Scope), token.Scope);
+            collector.Add(nameof(token.SessionState), token.SessionState);
+            collector.Add(nameof(token.TokenType), token.TokenType);
+        }
+    }
+
+    public static void RecordTags(ITagCollector collector, DateTimeOffset expiresAt)
+    {
+        collector.Add("ExpiresAt", expiresAt);
+    }
+
+    public static void RecordCacheKeyTag(ITagCollector collector, string cacheKey)
+    {
+        if (cacheKey is not null)
+        {
+            collector.Add("CacheKey", cacheKey);
+        }
+    }
+
+    public static void RecordTags(ITagCollector collector, HttpStatusCode httpStatus)
+    {
+        collector.Add("HttpStatusCode", httpStatus);
+    }
+}
