117aa7746392390bbf4c6c35e5da689eda0e4556
Phil Bolduc <philbolduc@gmail.com>
2024-03-06 13:43:05 -0800

    add more extensions, add ng auto completion to bash

--- src/frontend/citizen-portal/.devcontainer/post-create.sh
+++ src/frontend/citizen-portal/.devcontainer/post-create.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Install the Angular CLI autocompletion to the user's profile
+grep -q "Angular" $HOME/.bashrc 2> /dev/null
+if [ $? -ne 0 ]
+then
+  echo "# Load Angular CLI autocompletion." >> $HOME/.bashrc
+  echo "source <(ng completion script)" >> $HOME/.bashrc
+fi
 
 yarn install
