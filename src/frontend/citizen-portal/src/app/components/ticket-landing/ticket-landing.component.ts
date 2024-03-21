afe199e6cc3893f08f6aed5993306d09fbab963b
Shark Wong <wtsunwai@gmail.com>
2024-03-21 10:41:39 -0700

    TCVP-2776 styles update

--- src/frontend/citizen-portal/src/app/components/ticket-landing/ticket-landing.component.ts
+++ src/frontend/citizen-portal/src/app/components/ticket-landing/ticket-landing.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ticket-landing',
   templateUrl: './ticket-landing.component.html',
+  styleUrls: ['./ticket-landing.component.scss']
 })
 
 export class TicketLandingComponent {
