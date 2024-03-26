405a3c5ccb8014d1548852e594a54150afcf8ef4
Sai Krishna Metpalli <310911@NTTDATA.COM>
2024-03-26 15:01:21 -0700

    TCVP-2771: Added the front end paging and sorting

---
+++ src/frontend/staff-portal/src/app/components/paging/paging.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-paging',
+  templateUrl: './paging.component.html',
+  styleUrls: ['./paging.component.scss']
+})
+export class PagingComponent {
+  @Input() currentPage: number;
+  @Input() totalPages: number;
+  @Output() pageChanged: EventEmitter<number> = new EventEmitter();
+
+  changePage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.pageChanged.emit(page);
+    }
+  }
+
+  showPage(page: number): boolean {
+    if(this.currentPage === 1 && page <= 3) {
+      return true;
+    } else if(this.currentPage === this.totalPages && page >= this.totalPages - 2) {
+      return true;
+    } else if(this.currentPage > 1 && this.currentPage < this.totalPages && page >= this.currentPage - 1 && page <= this.currentPage + 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
