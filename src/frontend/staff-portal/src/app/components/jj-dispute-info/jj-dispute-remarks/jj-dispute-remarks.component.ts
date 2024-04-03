54640e95311337879843a8539b333560700cea49
Shark Wong <wtsunwai@gmail.com>
2024-04-03 12:13:21 -0700

    TCVP-2637 show comment in File Remarks

--- src/frontend/staff-portal/src/app/components/jj-dispute-info/jj-dispute-remarks/jj-dispute-remarks.component.ts
+++ src/frontend/staff-portal/src/app/components/jj-dispute-info/jj-dispute-remarks/jj-dispute-remarks.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnChanges, ViewChild, Input } from '@angular/core';
+import { Component, OnChanges, ViewChild, Input, OnDestroy } from '@angular/core';
 import { MatLegacyTableDataSource as MatTableDataSource } from '@angular/material/legacy-table';
 import { MatSort } from '@angular/material/sort';
-import { JJDisputeRemark } from 'app/api';
+import { FileHistory, JJDisputeRemark } from 'app/api';
+import { HistoryRecordService } from 'app/services/history-records.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-jj-dispute-remarks',
   templateUrl: './jj-dispute-remarks.component.html',
   styleUrls: ['./jj-dispute-remarks.component.scss'],
 })
-export class JJDisputeRemarksComponent implements OnChanges {
+export class JJDisputeRemarksComponent implements OnChanges, OnDestroy {
   @Input() data: JJDisputeRemark[];
   @ViewChild(MatSort) sort = new MatSort();
 
@@ -17,13 +19,37 @@ export class JJDisputeRemarksComponent implements OnChanges {
     "createdTs",
     "userFullName",
     "note",
-  ]
+  ];
+  subscriptions: Subscription[] = [];
 
   constructor(
+    private historyRecordService: HistoryRecordService
   ) {
+    this.subscriptions.push(this.historyRecordService.FileHistories$.subscribe(fileHistories => {
+      this.refreshData();
+    }));
   }
 
   ngOnChanges(): void {
+    this.refreshData();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    })
+  }
+
+  refreshData(): void {
+    // Add Record saving remarks
+    this.historyRecordService.FileHistories?.filter(i => i.auditLogEntryType === "FRMK").forEach((fileHistory: FileHistory) => {
+      this.data.push(<JJDisputeRemark>{
+        createdTs: fileHistory.createdTs,
+        userFullName: fileHistory.actionByApplicationUser,
+        note: fileHistory.comment
+      })
+    })
+
     this.data = this.data?.sort((a: JJDisputeRemark, b: JJDisputeRemark) => {
       return Date.parse(a.createdTs) - Date.parse(b.createdTs)
     });
