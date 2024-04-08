14f0150b80dcefb941b05ab04b6d49958bd6dc0e
Phil Bolduc <philbolduc@gmail.com>
2024-04-08 14:13:04 -0700

    Adjust maintenance site helm chart and values

--- gitops/charts/maintenance/deploy-maintenance-site.ps1
+++ gitops/charts/maintenance/deploy-maintenance-site.ps1
@@ -9,7 +9,8 @@ oc project $plate-$environment 2>&1 | Out-Null
 
 if ($LastExitCode -eq 0) {
   # install or upgrade the application
-  helm upgrade maintenance maintenance-site --install --values values-$environment.yaml
+  helm upgrade --install maintenance-site maintenance-site --values values-$environment.yaml
 } else {
     Write-Error "Could not change to project $plate-$environment"
 }
+
