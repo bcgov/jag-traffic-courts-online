f1f624d05787e4b8a6e4a28608fb67dcea60312b
Sai Krishna Metpalli <310911@NTTDATA.COM>
2024-05-24 09:04:53 -0700

    TCVP-2687 Fixed the role permissions

--- src/frontend/staff-portal/src/app/components/error/unauthorized/unauthorized.component.ts
+++ src/frontend/staff-portal/src/app/components/error/unauthorized/unauthorized.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorized',
   templateUrl: './unauthorized.component.html',
   styleUrls: ['./unauthorized.component.scss']
 })
-export class UnauthorizedComponent {
-  constructor() {
+export class UnauthorizedComponent implements OnInit {
+  application: string;
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.application = this.route.snapshot.queryParamMap.get('application');
   }
 }
