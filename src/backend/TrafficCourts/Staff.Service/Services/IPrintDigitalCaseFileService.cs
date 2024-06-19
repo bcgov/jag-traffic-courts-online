6eed73d398d4014523fc0fc0cf8b9afddc05bdf1
Burak Karahan <burak.karahan@nttdata.com>
2024-06-19 14:21:44 -0400

    TCVP-2987: Regenerated the api models based on the updated swagger.json

--- src/backend/TrafficCourts/Staff.Service/Services/IPrintDigitalCaseFileService.cs
+++ src/backend/TrafficCourts/Staff.Service/Services/IPrintDigitalCaseFileService.cs
@@ -8,7 +8,6 @@ public interface IPrintDigitalCaseFileService
     /// <summary>
     /// Renders the digital case file for a given dispute based on ticket number. This really should be using the tco_dispute.dispute_id.
     /// </summary>
-    Task<RenderedReport> PrintDigitalCaseFileAsync(string ticketNumber, string timeZoneId, DcfTemplateType type, CancellationToken cancellationToken);
     Task<RenderedReport> PrintDigitalCaseFileAsync(string ticketNumber, TimeZoneInfo timeZone, DcfTemplateType type, CancellationToken cancellationToken);
 
     /// <summary>
