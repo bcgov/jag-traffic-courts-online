d419cc13f56e46b5f49a46993456fd4f7e214811
Dave <dave.kay@number41media.com>
2025-03-19 11:45:27 -0700

    API V2 > OCCAM Disputes > Handler and datamodel updates

--- src/backend/TrafficCourts/Staff.Service/Features/Occam/Disputes/Handler.cs
+++ src/backend/TrafficCourts/Staff.Service/Features/Occam/Disputes/Handler.cs
@@ -2,6 +2,8 @@
 using System.Text;
 using TrafficCourts.Common;
 using TrafficCourts.OrdsDataService.Occam;
+using TrafficCourts.OrdsDataService.Tco;
+using TrafficCourts.Staff.Service.Features.CourtFiles.Summaries;
 
 namespace TrafficCourts.Staff.Service.Features.Occam.Disputes;
 
@@ -16,16 +18,59 @@ public class Handler : IRequestHandler<Request, Response>
 
     public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
     {
+        // todo: add parameters to the request
+
         var items = await _repository.GetListAsync(null, cancellationToken);
 
         var buffer = new StringBuilder();
         List<Domain.Models.DisputeCaseFileSummary> models = items.Select(_ => _.ToDomainModel()).ToList();
 
+        // Add these to the request instead, and do them DB side?
         if (request.Parameters != null)
         {
             // Do filtering? Why aren't we doing this DB side??!
         }
 
         return new Response(models);
+    }
+
+    private Response CreateResponse(OrdsDataService.OrdsDataServicePagedCollectionResponse<OrdsDisputeCaseFileSummary> pagedCollection)
+    {
+        if (pagedCollection.Rows is not null)
+        {
+            var items = pagedCollection.Rows.Select(Map);
+
+            var offset = pagedCollection.Offset;
+            var pageSize = pagedCollection.Fetch;
+            var totalRows = pagedCollection.TotalRows;
+
+            int pageNumber = (offset / pageSize) + 1;
+            int totalPages = (int)Math.Ceiling((double)totalRows / pageSize);
+
+            var pagedList = new PagedDisputeCaseFileSummaryCollection(items, pageNumber, pageSize, pagedCollection.TotalRows);
+            return new Response(pagedList);
+        }
+
+        // generate an error id that we log and return to the client
+        string errorId = Guid.NewGuid().ToString("n");
+
+        var error = pagedCollection.Errors?.FirstOrDefault();
+        var logger = _logger.ForContext("ErrorId", errorId);
+
+        if (error is not null)
+        {
+            logger
+                .ForContext("ErrorCode", error.ErrorCode)
+                .ForContext("ErrorMessage", error.ErrorMessage)
+                .ForContext("ErrorStack", error.ErrorStack)
+                .Error("Error fetching data from ORDS");
+        }
+        else
+        {
+            logger
+                .Warning("No data returned from ORDS, no error details are available");
+        }
+
+        return new Response(errorId);
     }
 }
--- src/backend/TrafficCourts/TrafficCourts.OrdsDataService/Occam/OccamDispute.cs
+++ src/backend/TrafficCourts/TrafficCourts.OrdsDataService/Occam/OccamDispute.cs
@@ -6,9 +6,30 @@
 
 public class OccamDispute
 {
-    public long dispute_id { get; set; }
+    public int dispute_id { get; set; }
+    public DateTime? submitted_dt { get; set; }
+    public string disputant_surname_nm { get; set; }
+    public string disputant_given_1_nm { get; set; }
+    public string disputant_given_2_nm { get; set; }
+    public string disputant_given_3_nm { get; set; }
+    public string email_address_txt { get; set; }
+    public string email_verified_yn { get; set; }
+    public string dispute_status_type_cd { get; set; }
+    public string request_court_appearance_yn { get; set; }
+    public string disputant_detect_ocr_issues_yn { get; set; }
+    public string system_detect_ocr_issues_yn { get; set; }
+    public string interpreter_required_yn { get; set; }
+    public string user_assigned_to { get; set; }
+    public string filing_dt { get; set; }
+    public float? court_agen_id { get; set; }
+    public string court_agen_nm { get; set; }
+    public int? violation_ticket_upload_id { get; set; }
+    public string ticket_number_txt { get; set; }
+    public DateTime? violation_dt { get; set; }
+    public string jj_assigned_to { get; set; }
+    public string most_recent_decision_made_by { get; set; }
+    public string jj_decision_dt { get; set; }
 }
-
 
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
 #pragma warning restore IDE1006 // Naming Styles
