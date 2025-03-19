b143cc0a47e0cc77b86c0d961eb1c868d687c0ba
Phil Bolduc <philbolduc@gmail.com>
2025-03-19 16:31:20 -0700

    TCVP-3144 Add filter statutes by date

--- src/backend/TrafficCourts/Citizen.Service/Services/Lookups/StatuteLookupService.cs
+++ src/backend/TrafficCourts/Citizen.Service/Services/Lookups/StatuteLookupService.cs
@@ -1,52 +1,59 @@
-﻿using System.Text;
+﻿using Serilog;
+using System.Text;
 using TrafficCourts.Common;
 using TrafficCourts.OrdsDataService.Justin;
+
+using ILogger = Serilog.ILogger;
 
 namespace TrafficCourts.Citizen.Service.Services.Lookups;
 
 public class StatuteLookupService : IStatuteLookupService
 {
     private readonly IStatuteRepository _repository;
+    private readonly ILogger _logger;
 
-    public StatuteLookupService(IStatuteRepository repository)
+    public StatuteLookupService(IStatuteRepository repository, ILogger logger)
     {
         _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        _logger = logger;
     }
 
     public async Task<TrafficCourts.Domain.Models.Statute?> GetBySectionAsync(string section, CancellationToken cancellationToken)
     {
-        var items = await _repository.GetListAsync(cancellationToken);
-
-        var buffer = new StringBuilder();
-        var models = items.Select(_ => _.ToDomainModel(buffer)).ToList();
-
+        // this is expensive to fetch all the records. Ideally we would split the section into the act code, section code, etc to find
+        // the matching record. For now, we will fetch all the records and find the matching record.
+        var models = await GetListAsync(cancellationToken);
         return models.FirstOrDefault(x => x.Code == section);
     }
 
     public async Task<TrafficCourts.Domain.Models.Statute?> GetByIdAsync(int statuteId, CancellationToken cancellationToken)
     {
-        var items = await _repository.GetListAsync(cancellationToken);
+        List<Statute> items = await _repository.GetListAsync(cancellationToken);
 
-        var index = items.BinarySearch(
-            new Statute { stat_id = statuteId },
-            new StatuteIdComparer());
+        var model = items.SingleOrDefault(x => x.stat_id == statuteId);
 
-        if (index >= 0)
+        if (model is null)
         {
-            return items[index].ToDomainModel(new StringBuilder());
+            _logger.Information("Statute with ID {StatuteId} not found", statuteId);
+            return null;
         }
-
-        return null;
+        else
+        {
+            return model.ToDomainModel(new StringBuilder());
+        }
     }
 
     public async Task<IList<TrafficCourts.Domain.Models.Statute>> GetListAsync(CancellationToken cancellationToken)
     {
-        var items = await _repository.GetListAsync(cancellationToken);
+        var models = await GetListAsync(DateTime.Today, cancellationToken);
+        return models;
+    }
 
+    public async Task<IList<TrafficCourts.Domain.Models.Statute>> GetListAsync(DateTime effectiveOn, CancellationToken cancellationToken)
+    {
+        var items = await _repository.GetListAsync(effectiveOn, cancellationToken);
         var buffer = new StringBuilder();
-
         var models = items.Select(x => x.ToDomainModel(buffer)).ToList();
-
         return models;
     }
 }
