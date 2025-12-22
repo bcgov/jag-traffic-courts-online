773193fb324a706c481d0a09f89535bac829cb52
Dave <dave.kay@number41media.com>
2025-12-22 10:20:42 -0800

    Stubbed endpoint for Dispute Save

--- src/backend/TrafficCourts/Staff.Service/Controllers/DisputeController.cs
+++ src/backend/TrafficCourts/Staff.Service/Controllers/DisputeController.cs
@@ -727,4 +727,66 @@ public class DisputeController : StaffControllerBase
             return new HttpError(StatusCodes.Status500InternalServerError, e.Message);
         }
     }
+
+    /// <summary>
+    /// Creates a Dispute record, similar to a Citizen Dispute Submit, but from Staff instead
+    /// </summary>
+    /// <param name="disputeId">Unique identifier for a specific Dispute record.</param>
+    /// <param name="dispute"></param>
+    /// <param name="timeZone"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    /// <response code="200">The Dispute is updated.</response>
+    /// <response code="400">The request was not well formed. Check the parameters.</response>
+    /// <response code="401">Request lacks valid authentication credentials.</response>
+    /// <response code="403">Forbidden, requires dispute:update permission.</response>
+    /// <response code="404">The Dispute to update was not found.</response>
+    /// <response code="409">The Dispute has already been assigned to a user. Dispute cannot be modified until assigned time expires.</response>
+    /// <response code="500">There was a server error that prevented the update from completing successfully.</response>
+    [HttpPut("{disputeId}")]
+    [ProducesResponseType(typeof(Dispute), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [KeycloakAuthorize(Resources.Dispute, Scopes.Update)]
+    public async Task<IActionResult> CreateDisputeAsync(long disputeId,
+        Dispute dispute,
+        [FromHeader(Name = "X-Timezone")] string timeZone,
+        CancellationToken cancellationToken)
+    {
+        if (!ValidateTimeZone(timeZone, out TimeZoneInfo? timeZoneInfo, out IActionResult? validationResult))
+        {
+            return validationResult; // Return BadRequest if validation fails
+        }
+
+        _logger.LogDebug("Creating the Dispute in oracle-data-api");
+
+        try
+        {
+            Dispute updatedDispute = await _disputeService.CreateDisputeAsync(User, dispute, timeZoneInfo, cancellationToken);
+            return Ok(updatedDispute);
+        }
+        catch (ApiException e) when (e.StatusCode == StatusCodes.Status400BadRequest)
+        {
+            return new HttpError(e.StatusCode, e.Message);
+        }
+        catch (ApiException e) when (e.StatusCode == StatusCodes.Status404NotFound)
+        {
+            return new HttpError(e.StatusCode, e.Message);
+        }
+        catch (ApiException e)
+        {
+            _logger.LogError(e, "Error retrieving Dispute from oracle-data-api");
+            return new HttpError(StatusCodes.Status500InternalServerError, e.Message);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error updating Dispute in oracle-data-api");
+            return new HttpError(StatusCodes.Status500InternalServerError, e.Message);
+        }
+    }
+
 }
